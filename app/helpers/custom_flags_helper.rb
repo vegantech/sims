07bacfff9e02b3aed1b4b6e605e6805b54a68ecc
Shawn Balestracci <shawn@vegantech.com>
2008-10-05 19:36:25 -0700

    display of custom flags in profile and list

--- app/helpers/custom_flags_helper.rb
+++ app/helpers/custom_flags_helper.rb
@@ -1,2 +1,40 @@
 module CustomFlagsHelper
+  #These will go in system flags when it is created
+
+  def displayflag(image,popup,flagtype, student)
+    image_tag(image,"onmouseover" => "return overlib('#{popup}');",
+      "onmouseout" => "return nd();") + " "
+  end
+
+  def status_display(student, change=nil)
+    str = ''
+    student.flags.current.each do |flagtype,flags|
+      popup="#{Flag::FLAGTYPES[flagtype][:icon].split('.').first.upcase}: #{flags.collect(&:summary).join(" ")}"
+      str += displayflag(Flag::FLAGTYPES[flagtype][:icon],popup, flagtype, student)
+      
+    end
+
+    if change.nil? && student.flags.any?
+      popup="C: Custom Flags- #{student.flags.custom_summary}"
+      str += image_tag("C.gif",
+      "onmouseover" => "return overlib('#{popup}');",
+      "onmouseout" => "return nd();")
+    end
+
+    str
+
+  end
+
+
+  def flag_select
+    Flag::TYPES.inject(''){|result,flagtype| result += flag_checkbox(flagtype)}
+  end
+
+  def flag_checkbox(flagtype)
+    f=Flag::TYPES[flagtype.to_s]
+    check_box_tag("flagged_intervention_type[]", flagtype, false,:id=>"flag_#{flagtype}", :onclick=>"searchByFlag()") +
+    content_tag(:label, image_tag(f[:icon], :title=>f[:humanize]), {'for' => "flag_#{flagtype}"})
+    
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,25 @@ class Student < ActiveRecord::Base
   belongs_to :district
   has_many :enrollments
   has_many :schools, :through=>:enrollments
+  has_many :flags do
+    def custom_summary
+      find_all_by_type('CustomFlag').collect(&:summary)
+    end
+    
+    def current
+      group_by(&:category)
+    end
+  end
+
+  #This is duplicated in user
+  def fullname 
+    first_name.to_s + ' ' + last_name.to_s
+  end
+
+  def fullname_last_first
+    last_name.to_s + ', ' + first_name.to_s
+  end
+
+
+
 end
