276a6e0b5bfa1e52205f5c71878d176e78f9cf5c
Shawn Balestracci <shawn@vegantech.com>
2008-10-17 21:10:06 -0700

    recommended monitor assignment and movement

---
+++ app/controllers/intervention_builder/recommended_monitors_controller.rb
@@ -0,0 +1,49 @@
+class InterventionBuilder::RecommendedMonitorsController < ApplicationController
+
+  helper_method :move_path
+  def assign_probes_to_intervention
+    
+    @intervention_definition=current_district.find_intervention_definition_by_id(params[:id])
+  
+    if request.post? and params[:commit]
+      flash[:notice=] = "Changes saved for #{@intervention_definition.title}" if @intervention_definition.probe_definition_ids=params[:probes]
+      redirect_to intervention_builder_intervention_url(*@intervention_definition.ancestor_ids) and return        
+    end
+    @recommended_monitors = @intervention_definition.recommended_monitors.collect(&:probe_definition_id)
+    @probe_definitions_in_groups =  current_district.probe_definitions.group_by_cluster_and_objective
+ 
+  end
+
+  def assign_interventions_to_probe
+    @probe_definition=current_district.probe_definitions.find(params[:id])
+  
+    if request.post? and params[:commit]
+      flash[:notice=] = "Changes saved for #{@probe_definition.title}" if @probe_definition.intervention_definition_ids=params[:int_defs]
+      redirect_to intervention_builder_probes_url and return        
+    end
+    @recommended_monitors = @probe_definition.recommended_monitors.collect(&    :intervention_definition_id)
+    @goal_definitions=current_district.goal_definitions
+  end
+ 
+  def move
+    @recommended_monitor = current_district.recommended_monitors.find(params[:id])
+
+    if params[:direction]
+      @recommended_monitor.move_higher if params[:direction].to_s == "up"
+      @recommended_monitor.move_lower if params[:direction].to_s == "down"
+    end
+    respond_to do |format|
+      format.html {redirect_to intervention_bulder_intervention_url(
+        *@recommended_monitor.intervention_definition.ancestor_ids)}
+      format.js {@recommended_monitors=@recommended_monitor.intervention_definition.recommended_monitors} 
+    end
+  end
+
+  def move_path(item, direction)
+     url_for(:controller=>"recommended_monitors",:action=>:move,:direction=>direction,:id=>item)
+  end
+
+
+
+end
+
--- app/models/district.rb
+++ app/models/district.rb
@@ -4,11 +4,17 @@ class District < ActiveRecord::Base
   has_many :checklist_definitions
   has_many :goal_definitions, :order=>:position
   has_many :probe_definitions
+  has_many :recommended_monitors, :through => :probe_definitions
+  has_many :objective_definitions, :through => :goal_definitions
 
   GRADES=  %w{ PK KG 01 02 03 04 05 06 07 08 09 10 11 12}
 
   def grades
     GRADES
+  end
+
+  def find_intervention_definition_by_id(id)
+    InterventionDefinition.find(id,:include=>{:intervention_cluster=>{:objective_definition=>:goal_definition}}, :conditions=>{'goal_definitions.district_id'=>self.id})
   end
 
 end
