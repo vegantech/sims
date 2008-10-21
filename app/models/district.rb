34f4dcf260f8522a2be8071a55714fb00547ac1c
Shawn Balestracci <shawn@vegantech.com>
2008-10-21 16:13:59 -0700

    column name in order needs to be a string

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,7 +2,7 @@ class District < ActiveRecord::Base
   belongs_to :state
   has_many :users
   has_many :checklist_definitions
-  has_many :goal_definitions, :order=>:position
+  has_many :goal_definitions, :order=>'position'
   has_many :probe_definitions
   has_many :recommended_monitors, :through => :probe_definitions
   has_many :objective_definitions, :through => :goal_definitions
