d7ac81001b2fa471a589e2b02e0676b37b72e068
Shawn Balestracci <shawn@vegantech.com>
2008-10-21 16:05:15 -0700

    remove unused methods from students controller and tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -62,69 +62,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # GET /students/new
-  # GET /students/new.xml
-  def new
-    @student = current_school.students.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @student }
-    end
-  end
-
-  # GET /students/1/edit
-  def edit
-    @student = current_schools.students.find(params[:id])
-  end
-
-  # POST /students
-  # POST /students.xml
-  def create
-    @student = current_schools.students.build(params[:student])
-
-    respond_to do |format|
-      if @student.save
-        flash[:notice] = 'Student was successfully created.'
-        format.html { redirect_to(@student) }
-        format.xml  { render :xml => @student, :status => :created, :location => @student }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /students/1
-  # PUT /students/1.xml
-  def update
-    @student = current_school.students.find(params[:id])
-
-    respond_to do |format|
-      if @student.update_attributes(params[:student])
-        flash[:notice] = 'Student was successfully updated.'
-        format.html { redirect_to(@student) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @student.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /students/1
-  # DELETE /students/1.xml
-  def destroy
-    @student = current_school.students.find(params[:id])
-    @student.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(students_url) }
-      format.xml  { head :ok }
-    end
-  end
-
-
   private
   def enforce_session_selections
     return true unless params[:id] 
--- test/functional/students_controller_test.rb
+++ test/functional/students_controller_test.rb
@@ -3,45 +3,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class StudentsControllerTest < ActionController::TestCase
 
   def test_should_get_index
+    #FIXME move this to rspec and add rest of tests
     get :index, {}, {:school_id=>schools(:alpha).id}
     assert_response :success
     assert_not_nil assigns(:students)
   end
 
-  def test_should_get_new
-
-    get :new,{},{:school_id=>schools(:alpha).id}
-    assert_response :success
-  end
-
-  def test_should_create_student
- #   assert_difference('Student.count') do
- #     post :create, :student => { }
- #   end
-
-#    assert_redirected_to student_path(assigns(:student))
-  end
-
-  def test_should_show_student
- #   get :show, {:id => students(:one).id}, :selected_students=>[students(:one).id]
- #   assert_response :success
-  end
-
-  def test_should_get_edit
-#    get :edit, {:id => students(:one).id}, :selected_students=>[students(:one).id]
-#    assert_response :success
-  end
-
-  def test_should_update_student
-    put :update, {:id => students(:one).id, :student => { }}, :selected_students=>[students(:one).id]
-    assert_redirected_to student_path(assigns(:student))
-  end
-
-  def test_should_destroy_student
-#    assert_difference('Student.count', -1) do
-#      delete :destroy, {:id => students(:one).id}, :selected_students=>[students(:one).id]
-#    end
-
-#    assert_redirected_to students_path
-  end
 end
