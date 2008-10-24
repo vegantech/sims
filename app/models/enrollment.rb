aac441c4988e6465cb480b462a7326d6ad0c650f
Shawn Balestracci <shawn@vegantech.com>
2008-10-24 12:20:21 -0700

    refactored enrollment model (and reformatted) working on adjusting the specs.   school_id should not be a search field

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,41 +2,41 @@ class Enrollment < ActiveRecord::Base
   belongs_to :student
   belongs_to :school
 
-	def self.search search_hash
-		if search_hash and search_hash[:school_id]
-			enrollments = School.find(search_hash[:school_id]).enrollments(:include => :students)
+  def self.search(search_hash={})
+    enrollments = find(:all,:include => :student) #expected to already be scoped through school
 
-			if search_hash[:grade] and search_hash[:grade] != '*'
-				enrollments = enrollments.select{|e| e.grade == search_hash[:grade]}
-			end
+    if search_hash[:grade] and search_hash[:grade] != '*'
+      enrollments = enrollments.select{|e| e.grade == search_hash[:grade]}
+    end
 
-			if search_hash[:last_name] and ! search_hash[:last_name].empty?
-				enrollments = enrollments.select{|e| e.student.last_name =~ /^#{search_hash[:last_name]}/i}
-			end
+    if search_hash[:last_name] and ! search_hash[:last_name].empty?
+      enrollments = enrollments.select{|e| e.student.last_name =~ /^#{search_hash[:last_name]}/i}
+    end
 
-			# session[:search] ||= {}
-			# session[:search][:grade] = selected_grade
-			# session[:search][:last_name] = selected_last_name
-			# session[:search][:search_type] = search_type
-			# session[:search][:flagged_interention_types] = intervention_types
+    # session[:search] ||= {}
+    # session[:search][:grade] = selected_grade
+    # session[:search][:last_name] = selected_last_name
+    # session[:search][:search_type] = search_type
+    # session[:search][:flagged_interention_types] = intervention_types
 
-			case search_hash[:search_type]
-			when 'list_all'
-			when 'flagged_intervention'
-				intervention_types = search_hash[:flagged_intervention_types]
-				# only include enrollments for students who have at least one of the intervention types.
-				enrollments = enrollments.select do |e|
-					flags = e.student.flags.current.first
-					# puts "Found flags: #{flags.class.name}, #{flags.inspect}"
-					flags and flags.find{|k,v| intervention_types.include?(k)}
-				end
-			else
-				raise 'Unrecognized search_type'
-			end
+    case search_hash[:search_type]
+    when 'list_all'
+    when 'flagged_intervention'
+      intervention_types = search_hash[:flagged_intervention_types]
+      enrollments=enrollments.select{|e| e.student.flags.any?}
+      # only include enrollments for students who have at least one of the intervention types.
+      
+      #this doesn't work yet.
 
-			enrollments
-		else
-			[]
-		end
-	end
+      #enrollments = enrollments.select do |e|
+      #  flags = e.student.flags.current.first
+      #  # puts "Found flags: #{flags.class.name}, #{flags.inspect}"
+      #  flags and flags.find{|k,v| intervention_types.include?(k)}
+      #end
+    else
+      raise 'Unrecognized search_type'
+    end
+
+    enrollments
+  end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -2,78 +2,77 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Enrollment do
 
-	describe 'search class method' do
+  describe 'search class method' do
 
-		describe 'passed no school_id' do
-			it 'should default to no enrollments' do
-				search_results = Enrollment.search({})
-				search_results.should == []
-			end
-		end
+    describe 'passed no search criteria' do
+      it 'should raise an exception' do
+        lambda {Enrollment.search}.should raise_error
+      end
+    end
 
-		describe 'passed a school_id' do
-			it 'should return students from that school' do
-				enrollments = '7 students'
-				school = School.new
-				school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
-				School.should_receive(:find).with(7).and_return(school)
-				search_results = Enrollment.search({:school_id => 7, :search_type => 'list_all'})
-				search_results.should == enrollments
-			end
-		end
+    describe 'passed list_all' do
+      it 'should return all students' do
+        enrollments = '7 students'
+        Enrollment.should_receive(:find).with(:all,:include=>:student).and_return(enrollments)
+        Enrollment.search(:search_type => 'list_all').should == enrollments
+    end
+  end
 
-		describe 'passed school_id and grade' do
-			it 'should return students from that grade in that school' do
-				enrollment1 = mock_enrollment :grade => '1'
-				enrollment2 = mock_enrollment :grade => '2'
-				enrollment3 = mock_enrollment :grade => '3'
-				enrollments = [enrollment1, enrollment2, enrollment3]
-				school = School.new
-				school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
+    describe 'passed list_all with grade' do
+      it 'should return students from that grade' do
+        pending
+        enrollment1 = mock_enrollment :grade => '1'
+        enrollment2 = mock_enrollment :grade => '2'
+        enrollment3 = mock_enrollment :grade => '3'
+        enrollments = [enrollment1, enrollment2, enrollment3]
+        school = School.new
+        school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
 
-				School.should_receive(:find).with(8).and_return(school)
+        School.should_receive(:find).with(8).and_return(school)
 
-				search_results = Enrollment.search({:school_id => 8, :grade => '3', :search_type => 'list_all'})
-				search_results.should == [enrollment3]
-			end
-		end
+        search_results = Enrollment.search({:school_id => 8, :grade => '3', :search_type => 'list_all'})
+        search_results.should == [enrollment3]
+      end
+    end
 
-		describe 'passed school_id and partial last name' do
-			it 'should return all students whose last name starts with a match' do
-				enrollment1 = mock_enrollment :student => mock_student(:last_name => 'Aagard')
-				enrollment2 = mock_enrollment :student => mock_student(:last_name => 'Beauregard')
-				enrollment3 = mock_enrollment :student => mock_student(:last_name => 'Beaumeister')
-				enrollment4 = mock_enrollment :student => mock_student(:last_name => 'Capitan')
-				enrollments = [enrollment1, enrollment2, enrollment3, enrollment4]
+    describe 'passed school_id and partial last name' do
+      it 'should return all students whose last name starts with a match' do
+        pending
+        enrollment1 = mock_enrollment :student => mock_student(:last_name => 'Aagard')
+        enrollment2 = mock_enrollment :student => mock_student(:last_name => 'Beauregard')
+        enrollment3 = mock_enrollment :student => mock_student(:last_name => 'Beaumeister')
+        enrollment4 = mock_enrollment :student => mock_student(:last_name => 'Capitan')
+        enrollments = [enrollment1, enrollment2, enrollment3, enrollment4]
 
-				school = School.new
-				school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
+        school = School.new
+        school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
 
-				School.should_receive(:find).with(8).and_return(school)
+        School.should_receive(:find).with(8).and_return(school)
 
-				search_results = Enrollment.search({:school_id => 8, :last_name => 'Beau', :search_type => 'list_all'})
+        search_results = Enrollment.search({:school_id => 8, :last_name => 'Beau', :search_type => 'list_all'})
 
-				search_results.should == [enrollment2, enrollment3]
-			end
-		end
+        search_results.should == [enrollment2, enrollment3]
+      end
+  end
 
-		describe 'passed school_id and partial last name' do
-			it 'should only match beginnings of last names' do
-				enrollment1 = mock_enrollment :student => mock_student(:last_name => 'Anderson')
-				enrollment2 = mock_enrollment :student => mock_student(:last_name => 'Beausonmeister')
-				enrollment3 = mock_enrollment :student => mock_student(:last_name => 'Songstrum')
-				enrollments = [enrollment1, enrollment2, enrollment3]
+    describe 'passed school_id and partial last name' do
+      it 'should only match beginnings of last names' do
+        pending
+        enrollment1 = mock_enrollment :student => mock_student(:last_name => 'Anderson')
+        enrollment2 = mock_enrollment :student => mock_student(:last_name => 'Beausonmeister')
+        enrollment3 = mock_enrollment :student => mock_student(:last_name => 'Songstrum')
+        enrollments = [enrollment1, enrollment2, enrollment3]
 
-				school = School.new
-				school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
+        school = School.new
+        school.should_receive(:enrollments).with(:include => :students).and_return(enrollments)
 
-				School.should_receive(:find).with(8).and_return(school)
+        School.should_receive(:find).with(8).and_return(school)
 
-				search_results = Enrollment.search({:school_id => 8, :last_name => 'son', :search_type => 'list_all'})
+        search_results = Enrollment.search({:school_id => 8, :last_name => 'son', :search_type => 'list_all'})
 
-				search_results.should == [enrollment3]
-			end
-		end
+        search_results.should == [enrollment3]
+      end
+    end
 
-	end
+  end
 end
