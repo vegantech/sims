f0ad60e13e897deae541d7537737f04db789947d
Shawn Balestracci <shawn@vegantech.com>
2008-11-01 14:27:29 -0700

    odd rspec mock error when run in groups, workaround

--- spec/controllers/interventions/probes_controller_spec.rb
+++ spec/controllers/interventions/probes_controller_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe Interventions::ProbesController do
   it_should_behave_like "an authenticated controller"
   
-  def in_filters
+  before do 
     student=mock_student
     @intervention=mock_intervention
     @intervention_probe_assignment = mock_model(InterventionProbeAssignment)
@@ -11,7 +11,7 @@ describe Interventions::ProbesController do
                                    :find=>@intervention_probe_assignment)
     student.stub_association!(:interventions,:find=>@intervention)
 
-    controller.should_receive(:current_student).and_return(student)
+    controller.should_receive(:current_student).at_least_once.and_return(student)
 
   end
 
@@ -22,7 +22,6 @@ describe Interventions::ProbesController do
   describe "responding to GET index" do
 
     it "should expose all probes as @probes and set other instance vars" do
-      pending
       @intervention_probe_assignment.should_receive(:probes).and_return([mock_probe])
       get :index
       assigns(:intervention).should == @intervention
@@ -36,7 +35,6 @@ describe Interventions::ProbesController do
   describe "responding to GET show" do
 
     it "should expose the requested probe as @probe" do
-      pending
 
       @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe)
       get :show, :id => "37"
@@ -48,7 +46,6 @@ describe Interventions::ProbesController do
   describe "responding to GET new" do
   
     it "should expose a new probe as @probe" do
-      pending
       @intervention_probe_assignment.stub_association!(:probes,:new=>mock_probe)
       get :new
       assigns[:probe].should equal(mock_probe)
@@ -59,7 +56,6 @@ describe Interventions::ProbesController do
   describe "responding to GET edit" do
   
     it "should expose the requested probe as @probe" do
-      pending
       @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe)
       get :edit, :id => "37"
       assigns[:probe].should equal(mock_probe)
@@ -72,14 +68,12 @@ describe Interventions::ProbesController do
     describe "with valid params" do
       
       it "should expose a newly created probe as @probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:new=>mock_probe(:save => true))
         post :create, :probe => {:these => 'params'}
         assigns(:probe).should equal(mock_probe)
       end
 
       it "should redirect to the created probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:new=>mock_probe(:save => true))
         post :create, :probe => {}
         response.should redirect_to(probe_url(@intervention,@intervention_probe_assignment,mock_probe))
@@ -90,14 +84,12 @@ describe Interventions::ProbesController do
     describe "with invalid params" do
 
       it "should expose a newly created but unsaved probe as @probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:new=>mock_probe(:save => false))
         post :create, :probe => {:these => 'params'}
         assigns(:probe).should equal(mock_probe)
       end
 
       it "should re-render the 'new' template" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:new=>mock_probe(:save => false))
         post :create, :probe => {}
         response.should render_template('new')
@@ -112,14 +104,12 @@ describe Interventions::ProbesController do
     describe "with valid params" do
 
       it "should update the requested probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe)
         mock_probe.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :id => "37", :probe => {:these => 'params'}
       end
 
       it "should expose the requested probe as @probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe(:update_attributes=>true))
         
         put :update, :id => "1"
@@ -127,7 +117,6 @@ describe Interventions::ProbesController do
       end
 
       it "should redirect to the probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe(:update_attributes => true))
         put :update, :id => "1"
         response.should redirect_to(probe_url(@intervention,@intervention_probe_assignment,mock_probe))
@@ -138,21 +127,18 @@ describe Interventions::ProbesController do
     describe "with invalid params" do
 
       it "should update the requested probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe)
         mock_probe.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :id => "37", :probe => {:these => 'params'}
       end
 
       it "should expose the probe as @probe" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe(:update_attributes => false))
         put :update, :id => "1"
         assigns(:probe).should equal(mock_probe)
       end
 
       it "should re-render the 'edit' template" do
-      pending
         @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe(:update_attributes => false))
         put :update, :id => "1"
         response.should render_template('edit')
@@ -165,14 +151,12 @@ describe Interventions::ProbesController do
   describe "responding to DELETE destroy" do
 
     it "should destroy the requested probe" do
-      pending
       @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe)
       mock_probe.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
   
     it "should redirect to the probes list" do
-      pending
       @intervention_probe_assignment.stub_association!(:probes,:find=>mock_probe(:destroy => true))
       delete :destroy, :id => "1"
       response.should redirect_to(probes_url(@intervention,@intervention_probe_assignment))
