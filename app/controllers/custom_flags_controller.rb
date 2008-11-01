efbdd39edf901f2b14618cdb5a0bce2d8994ec4c
Shawn Balestracci <shawn@vegantech.com>
2008-11-01 10:56:55 -0700

    added creation of custom flag to integration test demo

--- app/controllers/custom_flags_controller.rb
+++ app/controllers/custom_flags_controller.rb
@@ -3,26 +3,6 @@ class CustomFlagsController < ApplicationController
   # GET /custom_flags.xml
   before_filter :enforce_session_selections
 
-  def index
-    @custom_flags = CustomFlag.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @custom_flags }
-    end
-  end
-
-  # GET /custom_flags/1
-  # GET /custom_flags/1.xml
-  def show
-    @custom_flag = CustomFlag.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @custom_flag }
-    end
-  end
-
   # GET /custom_flags/new
   # GET /custom_flags/new.xml
   def new
@@ -36,14 +16,7 @@ class CustomFlagsController < ApplicationController
     end
   end
 
-  # GET /custom_flags/1/edit
-  def edit
-    @custom_flag = CustomFlag.find(params[:id])
-  end
-
-  # POST /custom_flags
-  # POST /custom_flags.xml
-  def create
+ def create
     @custom_flag = CustomFlag.new(params[:custom_flag])
 
     respond_to do |format|
@@ -55,23 +28,6 @@ class CustomFlagsController < ApplicationController
       else
         format.html { render :action => "new" }
         format.js { render :action=> "new" }
-        format.xml  { render :xml => @custom_flag.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /custom_flags/1
-  # PUT /custom_flags/1.xml
-  def update
-    @custom_flag = CustomFlag.find(params[:id])
-
-    respond_to do |format|
-      if @custom_flag.update_attributes(params[:custom_flag])
-        flash[:notice] = 'CustomFlag was successfully updated.'
-        format.html { redirect_to(@custom_flag) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
         format.xml  { render :xml => @custom_flag.errors, :status => :unprocessable_entity }
       end
     end
