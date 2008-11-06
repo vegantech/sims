c38b91a8c3f1fad4680bd08956dd6268458b75f3
Shawn Balestracci <shawn@vegantech.com>
2008-11-06 14:06:47 -0800

    enable railmail [#49]

--- config/environment.rb
+++ config/environment.rb
@@ -70,4 +70,5 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+  config.actionmailer.deliverymethod = :railmail 
 end
