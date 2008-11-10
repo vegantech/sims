83ab128d28da449572d6fd3994e97fb128bc23dc
Shawn Balestracci <shawn@vegantech.com>
2008-11-10 12:19:59 -0800

    Will have to rethink how I test email notifications,   load error in rspec when requiring the notifications.rb

--- features/steps/require_everything.rb
+++ features/steps/require_everything.rb
@@ -2,7 +2,8 @@
 
 def require_everything_in_app
   Dir.glob(RAILS_ROOT+"/app/**/*.rb").each do |rb|
-    require rb.split("app/").last
+    require rb.split("app/").last  unless rb.include?"models/notifications.rb"
+
   end
 end
 
