ca1bd5037bea7aafbf7a3de013fb47138d2e51e6
Shawn Balestracci <shawn@vegantech.com>
2008-11-11 11:33:22 -0800

    intervention participant scaffolded spec runs now

--- spec/models/intervention_participant_spec.rb
+++ spec/models/intervention_participant_spec.rb
@@ -3,8 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe InterventionParticipant do
   before(:each) do
     @valid_attributes = {
-      :intervention => ,
-      :user => ,
       :role => "1"
     }
   end
