b9639e211f7a52e9756fc5009873bcbaae3a76d7
Shawn Balestracci <shawn@vegantech.com>
2008-11-25 09:13:44 -0800

    [#83] fixed and enabled specs for intervention participants controller

--- app/controllers/interventions/participants_controller.rb
+++ app/controllers/interventions/participants_controller.rb
@@ -1,26 +1,5 @@
 class Interventions::ParticipantsController < ApplicationController
-  # GET /intervention_participants
-  # GET /intervention_participants.xml
-  def index
-    @intervention_participants = InterventionParticipant.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @intervention_participants }
-    end
-  end
-
-  # GET /intervention_participants/1
-  # GET /intervention_participants/1.xml
-  def show
-    @intervention_participant = InterventionParticipant.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @intervention_participant }
-    end
-  end
-
+  before_filter :load_intervention
   # GET /intervention_participants/new
   # GET /intervention_participants/new.xml
   def new
@@ -45,7 +24,7 @@ class Interventions::ParticipantsController < ApplicationController
     respond_to do |format|
       if @intervention_participant.save
         flash[:notice] = 'InterventionParticipant was successfully created.'
-        format.html { redirect_to(@intervention_participant) }
+        format.html { redirect_to(@intervention) }
         format.xml  { render :xml => @intervention_participant, :status => :created, :location => @intervention_participant }
       else
         format.html { render :action => "new" }
@@ -62,7 +41,7 @@ class Interventions::ParticipantsController < ApplicationController
     respond_to do |format|
       if @intervention_participant.update_attributes(params[:intervention_participant])
         flash[:notice] = 'InterventionParticipant was successfully updated.'
-        format.html { redirect_to(@intervention_participant) }
+        format.html { redirect_to(@intervention) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -78,8 +57,14 @@ class Interventions::ParticipantsController < ApplicationController
     @intervention_participant.destroy
 
     respond_to do |format|
-      format.html { redirect_to(intervention_participants_url) }
+      format.html { redirect_to(@intervention) }
       format.xml  { head :ok }
     end
   end
+
+  protected
+  def load_intervention
+    @intervention=current_student.interventions.find(params[:intervention_id])
+  end
+
 end
--- spec/controllers/interventions/participants_controller_spec.rb
+++ spec/controllers/interventions/participants_controller_spec.rb
@@ -7,54 +7,19 @@ describe Interventions::ParticipantsController do
   def mock_intervention_participant(stubs={})
     @mock_intervention_participant ||= mock_model(InterventionParticipant, stubs)
   end
-  
-  describe "responding to GET index" do
 
-    it "should expose all intervention_participants as @intervention_participants" do ;pending
-      InterventionParticipant.should_receive(:find).with(:all).and_return([mock_intervention_participant])
-      get :index
-      assigns[:intervention_participants].should == [mock_intervention_participant]
-    end
-
-    describe "with mime type of xml" do
-  
-      it "should render all intervention_participants as xml" do ;pending
-        request.env["HTTP_ACCEPT"] = "application/xml"
-        InterventionParticipant.should_receive(:find).with(:all).and_return(intervention_participants = mock("Array of InterventionParticipants"))
-        intervention_participants.should_receive(:to_xml).and_return("generated XML")
-        get :index
-        response.body.should == "generated XML"
-      end
-    
-    end
-
+  before :each do 
+    student=mock_student
+    @intervention=mock_intervention
+    student.stub_association!(:interventions,:find=>@intervention)
+    controller.stub!(:current_student).and_return(student)
   end
-
-  describe "responding to GET show" do
-
-    it "should expose the requested intervention_participant as @intervention_participant" do ;pending
-      InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
-      get :show, :id => "37"
-      assigns[:intervention_participant].should equal(mock_intervention_participant)
-    end
-    
-    describe "with mime type of xml" do
-
-      it "should render the requested intervention_participant as xml" do ;pending
-        request.env["HTTP_ACCEPT"] = "application/xml"
-        InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
-        mock_intervention_participant.should_receive(:to_xml).and_return("generated XML")
-        get :show, :id => "37"
-        response.body.should == "generated XML"
-      end
-
-    end
-    
-  end
+ 
+  
 
   describe "responding to GET new" do
   
-    it "should expose a new intervention_participant as @intervention_participant" do ;pending
+    it "should expose a new intervention_participant as @intervention_participant" do 
       InterventionParticipant.should_receive(:new).and_return(mock_intervention_participant)
       get :new
       assigns[:intervention_participant].should equal(mock_intervention_participant)
@@ -64,7 +29,7 @@ describe Interventions::ParticipantsController do
 
   describe "responding to GET edit" do
   
-    it "should expose the requested intervention_participant as @intervention_participant" do ;pending
+    it "should expose the requested intervention_participant as @intervention_participant" do 
       InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
       get :edit, :id => "37"
       assigns[:intervention_participant].should equal(mock_intervention_participant)
@@ -76,29 +41,29 @@ describe Interventions::ParticipantsController do
 
     describe "with valid params" do
       
-      it "should expose a newly created intervention_participant as @intervention_participant" do ;pending
+      it "should expose a newly created intervention_participant as @intervention_participant" do 
         InterventionParticipant.should_receive(:new).with({'these' => 'params'}).and_return(mock_intervention_participant(:save => true))
         post :create, :intervention_participant => {:these => 'params'}
         assigns(:intervention_participant).should equal(mock_intervention_participant)
       end
 
-      it "should redirect to the created intervention_participant" do ;pending
+      it "should redirect to the created intervention_participant" do 
         InterventionParticipant.stub!(:new).and_return(mock_intervention_participant(:save => true))
         post :create, :intervention_participant => {}
-        response.should redirect_to(intervention_participant_url(mock_intervention_participant))
+        response.should redirect_to(intervention_url(@intervention))
       end
       
     end
     
     describe "with invalid params" do
 
-      it "should expose a newly created but unsaved intervention_participant as @intervention_participant" do ;pending
+      it "should expose a newly created but unsaved intervention_participant as @intervention_participant" do 
         InterventionParticipant.stub!(:new).with({'these' => 'params'}).and_return(mock_intervention_participant(:save => false))
         post :create, :intervention_participant => {:these => 'params'}
         assigns(:intervention_participant).should equal(mock_intervention_participant)
       end
 
-      it "should re-render the 'new' template" do ;pending
+      it "should re-render the 'new' template" do 
         InterventionParticipant.stub!(:new).and_return(mock_intervention_participant(:save => false))
         post :create, :intervention_participant => {}
         response.should render_template('new')
@@ -112,41 +77,41 @@ describe Interventions::ParticipantsController do
 
     describe "with valid params" do
 
-      it "should update the requested intervention_participant" do ;pending
+      it "should update the requested intervention_participant" do 
         InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
         mock_intervention_participant.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :id => "37", :intervention_participant => {:these => 'params'}
       end
 
-      it "should expose the requested intervention_participant as @intervention_participant" do ;pending
+      it "should expose the requested intervention_participant as @intervention_participant" do 
         InterventionParticipant.stub!(:find).and_return(mock_intervention_participant(:update_attributes => true))
         put :update, :id => "1"
         assigns(:intervention_participant).should equal(mock_intervention_participant)
       end
 
-      it "should redirect to the intervention_participant" do ;pending
+      it "should redirect to the intervention_participant" do 
         InterventionParticipant.stub!(:find).and_return(mock_intervention_participant(:update_attributes => true))
         put :update, :id => "1"
-        response.should redirect_to(intervention_participant_url(mock_intervention_participant))
+        response.should redirect_to(intervention_url(@intervention))
       end
 
     end
     
     describe "with invalid params" do
 
-      it "should update the requested intervention_participant" do ;pending
+      it "should update the requested intervention_participant" do 
         InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
         mock_intervention_participant.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :id => "37", :intervention_participant => {:these => 'params'}
       end
 
-      it "should expose the intervention_participant as @intervention_participant" do ;pending
+      it "should expose the intervention_participant as @intervention_participant" do 
         InterventionParticipant.stub!(:find).and_return(mock_intervention_participant(:update_attributes => false))
         put :update, :id => "1"
         assigns(:intervention_participant).should equal(mock_intervention_participant)
       end
 
-      it "should re-render the 'edit' template" do ;pending
+      it "should re-render the 'edit' template" do 
         InterventionParticipant.stub!(:find).and_return(mock_intervention_participant(:update_attributes => false))
         put :update, :id => "1"
         response.should render_template('edit')
@@ -158,16 +123,16 @@ describe Interventions::ParticipantsController do
 
   describe "responding to DELETE destroy" do
 
-    it "should destroy the requested intervention_participant" do ;pending
+    it "should destroy the requested intervention_participant" do 
       InterventionParticipant.should_receive(:find).with("37").and_return(mock_intervention_participant)
       mock_intervention_participant.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
   
-    it "should redirect to the intervention_participants list" do ;pending
+    it "should redirect to the intervention_participants list" do 
       InterventionParticipant.stub!(:find).and_return(mock_intervention_participant(:destroy => true))
       delete :destroy, :id => "1"
-      response.should redirect_to(intervention_participants_url)
+      response.should redirect_to(intervention_url(@intervention))
     end
 
   end
