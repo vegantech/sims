6416aec62e15f8338ffc530d39d52b06da5f2ced
Shawn Balestracci <shawn@vegantech.com>
2008-11-28 18:35:21 -0800

    some factory girl definitions

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,20 +1,66 @@
-require 'factory-girl'
+require 'factory_girl'
 
 Factory.sequence :username do |n|
-  "user#{n}"
+  "user#{n}#{Time.now.to_i}"
 end
 
 Factory.define :checklist do |c|
-  c.association, :student
-  c.association, :user
-  c.association, :tier
+  c.association :student
+  c.association :user
+  c.association :tier
+  c.association :checklist_definition
 end
 
 Factory.define :user do |u|
-  u.username {Factory.next}
+  u.username {Factory.next(:username)}
   u.passwordhash "password"
   u.last_name "Last_Name"
-  u.first_name {"First#{u.username}"}
+  u.first_name {|u| "First#{u.username}"}
 end
+
+Factory.define :user_with_default_roles do |u|
+  u.username {Factory.next(:username)}
+  u.passwordhash "password"
+  u.last_name "Last_Name"
+  u.first_name {|u| "First#{u.username}"}
+end
+
+Factory.define :checklist_definition do |c|
+  c.directions "Please folow the directions"
+  c.text "Text for Checklist"
+  c.association :question_definitions, :factory=>:question_definition
+end
+
+Factory.define :tier do |t|
+  t.title "Some tier"
+end
+
+Factory.define :student do |s|
+  s.last_name "Last"
+  s.first_name "First"
+end
+
+Factory.define :question_definition do |q|
+  q.text  "Question"
+  q.association :element_definitions, :factory =>:element_definition
+end
+
+Factory.define :element_definition do |e|
+  e.text  "Element"
+  e.kind  'relevant'
+  e.association :answer_definitions, :factory => :answer_definition
+end
+
+Factory.define :answer_definition do |a|
+  a.value 0
+end
+
+
+
+
+
+
+
+
   
 
