f3de1f24fc624a7251844624ed431140f0150f0c
Shawn Balestracci <shawn@vegantech.com>
2008-12-10 16:09:05 -0800

    should put us back over threshold for unit tests

---
+++ spec/lib/country_state_district_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+
+include CountryStateDistrict
+describe "Populate Country State and District dropdowns" do
+  describe 'dropdowns method' do
+    before do 
+     self.should_receive(:subdomains)
+     self.stub!(:params =>{})
+    end
+    it 'should return if logged in' do
+      Country.should_not_receive(:normal)
+      self.should_receive(:current_user_id).and_return(true)
+      dropdowns
+    end
+
+    it 'should populate countries states and districts if there are more than one' do
+      country=Country.new
+      state=State.new
+      district=District.new
+      country.stub_association!(:states,:normal=>[5,6,state])
+      state.stub_association!(:districts, :normal=>[district,7,8])
+      Country.should_receive(:normal).and_return([country,2,3])
+      self.should_receive(:current_user_id).and_return(false)
+      dropdowns
+      @countries.should == [country,2,3]
+      @country.should == country
+      @states.should == [5,6,state]
+      @state.should == state
+      @districts.should == [district,7,8]
+      @current_district.should == district
+    end
+
+
+
+
+  end 
+end
+
