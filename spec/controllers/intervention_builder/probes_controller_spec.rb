1532487b65c577ade5bfcd98a60613058f4e6035
Shawn Balestracci <shawn@vegantech.com>
2008-12-10 16:48:42 -0800

    added some specs for probes controller in intervention builder

--- spec/controllers/intervention_builder/probes_controller_spec.rb
+++ spec/controllers/intervention_builder/probes_controller_spec.rb
@@ -4,9 +4,168 @@ describe InterventionBuilder::ProbesController do
   it_should_behave_like "an authenticated controller"
   it_should_behave_like "an authorized controller"
 
-  #Delete this example and add some real ones
-  it "should use InterventionBuilder::ProbesController" do
-    controller.should be_an_instance_of(InterventionBuilder::ProbesController)
+  before do 
+    @district=mock_district
+    controller.stub!(:current_district=>@district)
+    @district.stub!(:probe_definitions=>ProbeDefinition)
+    @mock_probe_definition=mock_probe_definition
+  end
+  
+  describe "responding to GET index" do
+    it "should expose all probe_definitions as @probe_definitions_in_groups" do
+      ProbeDefinition.should_receive(:group_by_cluster_and_objective).and_return([1,2,3])
+      get :index
+      assigns[:probe_definitions_in_groups].should == [1,2,3]
+    end
+
+  end
+
+  describe "responding to GET show" do
+
+    it "should expose the requested probe_definition as @probe_definition" do
+      ProbeDefinition.should_receive(:find).with("37").and_return(@mock_probe_definition)
+      get :show, :id => "37"
+      assigns[:probe_definition].should equal(@mock_probe_definition)
+    end
+    
+   
+  end
+
+  describe "responding to GET new" do
+  
+    it "should expose a new probe_definition as @probe_definition" do
+      ProbeDefinition.should_receive(:build).and_return(@mock_probe_definition)
+      get :new
+      assigns[:probe_definition].should equal(@mock_probe_definition)
+    end
+
+  end
+
+  describe "responding to GET edit" do
+  
+    it "should expose the requested probe_definition as @probe_definition" do
+      ProbeDefinition.should_receive(:find).with("37").and_return(@mock_probe_definition)
+      get :edit, :id => "37"
+      assigns[:probe_definition].should equal(@mock_probe_definition)
+    end
+
+  end
+
+  describe "responding to POST create" do
+    before do
+      @district.should_receive(:probe_definitions).and_return(ProbeDefinition)
+
+    end
+
+    describe "with valid params" do
+      
+      it "should expose a newly created probe_definition as @probe_definition" do
+        pending
+        ProbeDefinition.should_receive(:build).with({'these' => 'params'}).and_return(mpd=mock_probe_definition(:save => true))
+        post :create, :probe_definition => {:these => 'params'}
+        assigns(:probe_definition).should equal(mpd)
+      end
+
+      it "should redirect to the created probe_definition" do
+        pending
+        ProbeDefinition.stub!(:build).and_return(mpd=mock_probe_definition(:save => true))
+        post :create, :probe_definition => {}
+        response.should redirect_to(intervention_builder_probes_url(mpd))
+      end
+      
+    end
+    
+    describe "with invalid params" do
+
+      it "should expose a newly created but unsaved probe_definition as @probe_definition" do
+        pending
+        ProbeDefinition.stub!(:build).with({'these' => 'params'}).and_return(mpd=mock_probe_definition(:save => false))
+        post :create, :probe_definition => {:these => 'params'}
+        assigns(:probe_definition).should equal(mpd)
+      end
+
+      it "should re-render the 'new' template" do
+        pending
+        ProbeDefinition.stub!(:build).and_return(mock_probe_definition(:save => false))
+        post :create, :probe_definition => {}
+        response.should render_template('new')
+      end
+      
+    end
+    
+  end
+
+  describe "responding to PUT udpate" do
+
+    describe "with valid params" do
+
+      it "should update the requested probe_definition" do
+        pending
+        ProbeDefinition.should_receive(:find).with("37").and_return(@mock_probe_definition)
+        @mock_probe_definition.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, :id => "37", :probe_definition => {:these => 'params'}
+      end
+
+      it "should expose the requested probe_definition as @probe_definition" do
+        pending
+        ProbeDefinition.stub!(:find).and_return(mpd=mock_probe_definition(:update_attributes => true))
+        put :update, :id => "1"
+        assigns(:probe_definition).should equal(mpd)
+      end
+
+      it "should redirect to the probe_definition" do
+        pending
+        ProbeDefinition.stub!(:find).and_return(mpd=mock_probe_definition(:update_attributes => true))
+        put :update, :id => "1"
+        response.should redirect_to(intervention_builder_probe_definition_url(mpd))
+      end
+
+    end
+    
+    describe "with invalid params" do
+
+      it "should update the requested probe_definition" do
+        pending
+        ProbeDefinition.should_receive(:find).with("37").and_return(@mock_probe_definition)
+        @mock_probe_definition.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, :id => "37", :probe_definition => {:these => 'params'}
+      end
+
+      it "should expose the probe_definition as @probe_definition" do
+        pending
+        ProbeDefinition.stub!(:find).and_return(mpd=mock_probe_definition(:update_attributes => false))
+        put :update, :id => "1"
+        assigns(:probe_definition).should equal(mpd)
+      end
+
+      it "should re-render the 'edit' template" do
+        pending
+        ProbeDefinition.stub!(:find).and_return(mock_probe_definition(:update_attributes => false))
+        put :update, :id => "1"
+        response.should render_template('edit')
+      end
+
+    end
+
+  end
+
+  describe "responding to DELETE destroy" do
+    describe 'when there are no objective definitions' do
+      before do
+        @mock_probe_definition.should_receive(:objective_definitions).and_return([])
+      end
+
+      it "should destroy the requested probe_definition when there are no objective definitions and redirect" do
+        pending
+        ProbeDefinition.should_receive(:find).with("37").and_return(@mock_probe_definition)
+        
+        @mock_probe_definition.should_receive(:destroy)
+        delete :destroy, :id => "37"
+        response.should redirect_to(intervention_builder_probe_definitions_url)
+      end
+    
+   end
+
   end
 
 end
