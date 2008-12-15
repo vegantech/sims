1e3232c7ea378bf09b090ba722255cfbbe2df78f
Shawn Balestracci <shawn@vegantech.com>
2008-12-15 11:37:47 -0800

    just outputting something for the call

--- vendor/plugins/webrat/lib/webrat.rb
+++ vendor/plugins/webrat/lib/webrat.rb
@@ -1,1 +1,1 @@
-raise "#{ENV.inspect} #{$0.inspect} #{ARGV.inspect} WHY?"
+puts "I'm not sure why this is called, but the runcoderun build needs this file to exist.  it's called in rake gems:install"
