6db0ceb7410e80ba7f3d34de67ac7854564a1bb8
Shawn Balestracci <shawn@vegantech.com>
2008-12-23 20:39:57 -0600

    removed duplicate class definition,   don't commit temporary cut and pastes they can be hard to track down.

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -99,28 +99,3 @@ class InterventionsController < ApplicationController
   
 end
 
-
-
-class Interventions::DefinitionsController < ApplicationController
-  include PopulateInterventionDropdowns
- 
-  def index
-    flash.keep(:custom_intervention)
-    populate_goals
-  end
-
-  
-  def select
-    flash.keep(:custom_intervention)
-    respond_to do |format|
-      redirect_to new_intervention_url(:goal_id=>params[:goal_id],:objective_id=>params[:objective_id],
-                                                    :category_id=>params[:category_id],:definition_id=>params[:intervention_definition][:id])
-      format.js {
-        populate_intervention
-     }
-    end
-  end
-
-end
-
-
