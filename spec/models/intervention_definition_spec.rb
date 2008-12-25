5172b00c662cfeb090d0764600c3ad8c32692971
Shawn Balestracci <shawn@vegantech.com>
2008-12-25 14:05:04 -0600

    Changed return value of district_quicklist to boolean,  had to change spec as well

--- spec/models/intervention_definition_spec.rb
+++ spec/models/intervention_definition_spec.rb
@@ -33,19 +33,19 @@ describe InterventionDefinition do
     it 'should return true and false if not if it is a member of the quicklist' do
       k=Factory(:intervention_definition)
       ql=QuicklistItem.create!(:district=>k.district, :intervention_definition => k)
-      k.district_quicklist.should_not == nil
+      k.district_quicklist.should == true
 
       ql.destroy
-      k.reload.district_quicklist.should == nil
+      k.reload.district_quicklist.should == false
     end
 
     it 'should assign itself to the quicklist if = "1"' do
       k=Factory(:intervention_definition)
-      k.district_quicklist.should == nil
+      k.district_quicklist.should == false
       k.district_quicklist = "1"
       k.save
       k.district.quicklist_items.first.intervention_definition.should == k
-      k.district_quicklist.should_not == nil
+      k.district_quicklist.should == true
     end
 
     it 'should remove itself from the quicklist if = false' do
@@ -54,11 +54,11 @@ describe InterventionDefinition do
       k.save
       k.district_quicklist ="0"
       k.save
-      k.district_quicklist.should == nil
+      k.district_quicklist.should == false
       k.district.quicklist_items.should == []
       k.reload
       k.district_quicklist =false
-      k.district_quicklist.should == nil
+      k.district_quicklist.should == false
     end
   end
 
