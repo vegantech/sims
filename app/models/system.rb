b3e68b391dc6287c4f2ffbb972d7e0d5994e674f
Shawn Balestracci <shawn@vegantech.com>
2008-12-29 11:09:10 -0800

    system admin login from scratch

--- app/models/system.rb
+++ app/models/system.rb
@@ -10,6 +10,14 @@ class System
   def self.bootstrap
     if Country.count == 0
       Country.create!(:admin=>true,:name=>"System Administration", :abbrev=>:admin)
+
+    end
+
+    if Role.count == 0
+      r=Role.create!(:name=>"country_admin")
+      r.rights.create!(:controller=>"countries", :read=>true, :write=>true)
+      Country.first.admin_district.users.first.roles << r
+
     end
 
 
