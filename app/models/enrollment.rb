31537fa58410b7927db8c10858c823a2348b737d
Shawn Balestracci <shawn@vegantech.com>
2008-12-29 18:09:38 -0600

    [#16] refactored search again with anonymous scope

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -19,56 +19,45 @@ class Enrollment < ActiveRecord::Base
 
   named_scope :by_student_ids_or_grades, lambda {|student_ids,grades| {:conditions => ["enrollments.student_id in (?) or enrollments.grade in (?)", Array(student_ids),Array(grades)]}}
 
-  named_scope :by_last_name, lambda {|last_name| {:conditions => ["students.last_name like ?","%#{last_name}%"]}}
-
-  named_scope :without_active_interventions,  :conditions=> ["not exists select * from interventions where interventions.student_id = students.id and interventions.active = ?",true]
   def self.search(search_hash = {})
     search_hash.symbolize_keys!
 
-    conditions ={}
-    conditions[:grade] = search_hash[:grade] if search_hash[:grade] and search_hash[:grade] != "*"
-    
-    
-   
-    includes=[:student]
-    
+    scope = self.scoped({:include=>:student})
+    scope =scope.scoped :conditions => {:grade=> search_hash[:grade]} if search_hash[:grade] and search_hash[:grade] != "*"
+    scope =scope.scoped :joins => "inner join groups_students on groups_students.student_id = students.id", :conditions => {"groups_students.group_id" => search_hash[:group_id]} unless search_hash[:group_id].blank? or search_hash[:group_id] == "*"
+    scope = scope.scoped :conditions => ["students.last_name like ?", "%#{search_hash[:last_name]}%"] unless search_hash[:last_name].blank?
 
-    unless search_hash[:group_id].blank? or search_hash[:group_id] == "*"
-      #inner join to only include students with matching groups using the habtm join table
-      joins = "inner join groups_students on groups_students.student_id = students.id"
-      conditions["groups_students.group_id"] = search_hash[:group_id]
-    end
-    
-    
-    
-    enrollments=case search_hash[:search_type]
+    case search_hash[:search_type]
     when 'list_all'
-      by_last_name(search_hash[:last_name]).find(:all,:conditions => conditions, :joins=>joins, :include=>includes)
     when 'flagged_intervention'  
       # only include enrollments for students who have at least one of the intervention types.
-      enrollments=by_last_name(search_hash[:last_name]).find(:all,:conditions => conditions, :joins=>joins, :include=>includes)
-      intervention_types = search_hash[:flagged_intervention_types]
-      if intervention_types.blank? # needs a different name?
-        enrollments.select{|e| e.student.flags.current.any?}
-      else
-        enrollments.select do |e|
-           e.student.flags.current.find{|flag_name, flags_array| intervention_types.include?(flag_name)}
-        end
-      end
+      #TODO This is any flags at all, ignore flags count as positive flag hits
+      scope =scope.scoped :conditions => "exists (select * from flags where flags.student_id = students.id)", :include => {:student=>:flags}
+
+      #TODO rename this to just flag_types, also in controller and view
+      scope= scope.scoped :conditions => {"flags.category"=>search_hash[:flagged_intervention_types]} unless search_hash[:flagged_intervention_types].blank?
+      #enrollments.select do |e|
+          #   e.student.flags.current.find{|flag_name, flags_array| intervention_types.include?(flag_name)}
+          # end
     when 'active_intervention'
-      enrollments=by_last_name(search_hash[:last_name]).find(:all,:conditions => conditions, :joins=>joins, :include=>includes)
-      enrollments.select{|e| e.student.interventions.active.any?}
+       scope=scope.scoped :conditions=> ["exists (select * from interventions where interventions.student_id = students.id and interventions.active = ?)",true],:include =>{:student=>:interventions}
       unless search_hash[:intervention_group_types].blank?
-        enrollments = enrollments.select do |e|
+        #        scope = scope.scoped :conditions =>  ... ,
+        #:includes => {:intervention=>{:intervention_definition=>{:intervention_cluster=>{:objective_definition=>:goal_definition}}}}
+        # with_active_interventions_by_groups(search_hash[:intervention_group], search_hash[:intervention_group_types])
+        enrollments = scope.select do |e|
+          #12/29/08 Don't think this can be much better, it filters the above by selected flags, note that the intervention group
+          #is dynamic and a call on the model
           e.student.interventions.active.any?{|i| search_hash[:intervention_group_types].include?(i.send(search_hash[:intervention_group].tableize.singularize).id.to_s)}
         end
       end
 
     when 'no_intervention'
-        without_active_interventions.by_last_name(search_hash[:last_name]).find(:all,:conditions => conditions, :joins=>joins, :include=>includes)
+      scope = scope.scoped :conditions=> ["not exists (select * from interventions where interventions.student_id = students.id and interventions.active = ?)",true]
     else
       raise 'Unrecognized search_type'
     end
+    scope
   end
 
 
