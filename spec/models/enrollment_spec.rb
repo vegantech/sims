bca9f47635d57b7bc61e5a06dd5214e01ba25e9a
Stephen Anderson <stephen@bendycode.com>
2009-01-20 20:39:44 -0600

    Minor changes only.

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -15,7 +15,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Enrollment do
 
-
   describe 'search class method' do
     it 'should contain and maintain scope' do
       school=Factory.create(:school)
@@ -30,7 +29,6 @@ describe Enrollment do
         e1,e2,e3 = %w{1 2 3}.collect{|i| Enrollment.create! :grade=>i.to_s, :student_id=>99999, :school_id=>999}
         Enrollment.search(:search_type => 'list_all', :grade=>'3').should == [e3]
       end
-
     end
 
     describe 'with different user and user_id' do
@@ -38,7 +36,6 @@ describe Enrollment do
         pending
       end
     end
-    
 
     describe 'passed no search criteria' do
       it 'should raise an exception' do
@@ -88,10 +85,8 @@ describe Enrollment do
         end
 
         search_results = Enrollment.search(:last_name => 'son', :search_type => 'list_all')
-
         search_results.should == [e3]
       end
-
     end
 
     describe 'with or without interventions' do
@@ -104,7 +99,7 @@ describe Enrollment do
         Factory(:intervention,:student=>@e1.student,:active=>false)
         Factory(:intervention,:student=>@e2.student,:active=>true)
       end
-    
+
       describe 'passed not in intervention' do
         it 'should only return students that are not in an intervention' do
           search_results = Enrollment.search(:search_type => 'no_intervention')
@@ -122,25 +117,22 @@ describe Enrollment do
           search_results = Enrollment.search(:search_type => 'active_intervention',:intervention_group=>'ObjectiveDefinition',
                                              :intervention_group_types=>[])
           search_results.should == [@e2]
-   
         end
 
         it 'should return students in an active intervention with any of the selected checkboxes' do
           ob2=@e2.student.interventions.first.objective_definition.id.to_s
           Factory(:intervention,:student=>@e3.student,:active=>true)
-          ob3=@e3.student.interventions.first.objective_definition.id.to_s
-          
-          @e4=Factory(:student).enrollments.create!(:grade=>"1",:school_id=>999)
+          ob3 = @e3.student.interventions.first.objective_definition.id.to_s
+
+          @e4 = Factory(:student).enrollments.create!(:grade=>"1",:school_id=>999)
           Factory(:intervention,:student=>@e4.student,:active=>true)
 
           search_results = Enrollment.search(:search_type => 'active_intervention',:intervention_group=>'ObjectiveDefinition',
                                              :intervention_group_types=>[ob2,ob3])
-                                                       
+
           search_results.should == [@e2,@e3]
-   
         end
       end
- 
     end
 
     describe 'passed flagged_intervention' do
@@ -165,7 +157,7 @@ describe Enrollment do
 
       describe 'and some flagged interventions were selected' do
         it 'should return students with any of the selected flagged interventions' do
-    
+
           search_results = Enrollment.search( :search_type => 'flagged_intervention',
             :flagged_intervention_types => ['attendance', 'suspension'])
 
@@ -188,8 +180,5 @@ describe Enrollment do
         end
       end
     end
-
   end
-
-
 end
