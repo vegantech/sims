80e5adcd84e376e859cbb17fd4401d01b5a349f4
Shawn Balestracci <shawn@vegantech.com>
2009-01-27 18:06:03 -0600

    spec links and attachment

--- spec/helpers/links_and_attachments_spec.rb
+++ spec/helpers/links_and_attachments_spec.rb
@@ -30,11 +30,28 @@ describe LinksAndAttachmentsHelper do
   end
 
   describe 'links_and_attachments' do
-    it 'display links to links and attachments' do
-      pending
-                       
+    def link_to_with_icon(name, url)
+      "#{name} -- #{url}"
+    end
+    it 'display nothing when there are no assets' do
+      k=mock_object(:assets=>[])
+      links_and_attachments(k,:blink).should be_blank
+    end
+
+    it 'should display a link when the object has 1 asset with just a link' do
+      document = mock_object(:original_filename=>"original_filename", :url=>"new_doc_url")
+      
+      
+      empty_asset=mock_asset(:url=>nil, 'document?'=>false)
+      link_asset=mock_asset('document?' =>false, :name=> 'link_asset', :url => 'www.link_asset.ant')
+      attach_asset=mock_asset(:url=>nil, :document => document, 'document?'=>true)
+      both_asset=mock_asset(:name=>'both_asset', :url =>'www.both_asset.mil',:document => document, 'document?' => true)
+      object=mock_object(:assets=>[link_asset,both_asset,empty_asset,attach_asset])
+
+      links_and_attachments(object, :dict).should == "<dict>link_asset -- www.link_asset.ant</dict> <dict>both_asset -- www.both_asset.mil</dict><dict>original_filename -- new_doc_url</dict>  <dict>original_filename -- new_doc_url</dict>"
 
     end
+    
   end
 end
 
