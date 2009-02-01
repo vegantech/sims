e8538937dbeededeb8650d03d419e3e98a3789e4
Shawn Balestracci <shawn@vegantech.com>
2009-01-31 21:00:08 -0600

    working on code coverage for unit specs #104

--- spec/lib/populate_intervention_dropdowns_spec.rb
+++ spec/lib/populate_intervention_dropdowns_spec.rb
@@ -47,11 +47,81 @@ describe "Populate Intervention Dropdowns Module" do
     end
   end
 
-  # describe 'populate_intervention' do
-  #   it 'should add intervention_definition to params[:intervention]' do
-  #     params[:intervention][:intervention_definition].should be_nil
-  #     populate_intervention
-  #     params[:intervention][:intervention_definition].should_not be_nil
-  #   end
-  # end
+  describe 'populate_quicklist' do
+    it 'should not populate @quicklist_interventions for custom interventions' do
+      flash[:custom_intervention]=true
+      populate_quicklist.should == nil
+      @quicklist_intervention_definitions.should == nil
+    end
+
+    it 'should set @quicklist_intervention_definitions if not custom interventins' do
+      flash[:custom_intervention]=nil
+      populate_quicklist.should == []
+      @quicklist_intervention_definitions.should == []
+    end
+  end
+
+  describe 'populate_goals' do 
+    it 'should populate @goal_definitions' do
+      self.should_receive(:find_goal_definition).twice
+      populate_goals
+      @goal_definitions.should == []
+      @goal_definition = true
+      self.should_receive(:populate_objectives)
+      populate_goals
+    end
+  end
+
+  describe 'populate_objectives' do
+    it 'should populate @objective_definitions' do
+      self.should_receive(:find_objective_definition).twice
+      @goal_definition=mock_goal_definition(:objective_definitions => [])
+      populate_objectives
+      @objective_definitions.should == []
+      @objective_definition = true
+      self.should_receive(:populate_categories)
+      populate_objectives
+    end
+  end
+ 
+  describe 'populate_categories' do
+    it 'should populate @intervention_clusters' do
+      self.should_receive(:find_intervention_cluster).twice
+      @objective_definition=mock_objective_definition(:intervention_clusters => [])
+      populate_categories
+      @intervention_clusters.should == []
+      @intervention_cluster = true
+      self.should_receive(:populate_definitions)
+      populate_categories
+    end
+  end
+ 
+  describe 'populate_definitions' do
+    it 'should populate @intervention_definitions if not custom' do
+      self.should_receive(:find_intervention_definition)
+      @intervention_cluster=mock_intervention_cluster(:intervention_definitions => [])
+      populate_definitions
+      @intervention_definitions.should == []
+    end
+    it 'should populate @intervention_definition if custom' do
+      flash[:custom_intervention] = true
+      self.should_receive(:find_intervention_definition)
+      @intervention_cluster=mock_intervention_cluster(:intervention_definitions => InterventionDefinition)
+      InterventionDefinition.should_receive(:build).and_return "1"
+      self.should_receive(:populate_intervention)
+      populate_definitions
+    end
+  end
+ 
+
+   describe 'populate_intervention' do
+     it 'should add intervention_definition to params[:intervention]' do
+       self.should_receive(:build_from_session_and_params)
+       self.should_receive(:find_intervention_definition)
+       params[:intervention][:intervention_definition].should be_nil
+       @intervention_definition ="test"
+       populate_intervention
+       params[:intervention][:intervention_definition].should == "test"
+     end
+   end
 end
