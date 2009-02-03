a042f6b1ba6cab81f42ba79862fe2ea70b774f77
Shawn Balestracci <shawn@vegantech.com>
2009-02-03 15:42:02 -0600

    [#144] limit team notes report to authorized students

--- spec/reports/team_notes_report_spec.rb
+++ spec/reports/team_notes_report_spec.rb
@@ -4,8 +4,11 @@ describe TeamNotesReport do
   describe 'render_text' do
     it 'should generate correct text output' do
       student = Factory(:student, :first_name => 'This', :last_name => 'Student')
-
+      school = Factory(:school, :district_id =>student.district_id)
+      student.enrollments.create!(:school=>school, :grade => "05")
+      
       user = Factory(:user, :username => 'some_user', :first_name => 'Some', :last_name => 'User')
+      user.special_user_groups.create!(:grouptype=>SpecialUserGroup::ALL_STUDENTS_IN_DISTRICT, :district_id => school.district_id)
 
       start_date = Date.new(2008, 12, 11)
       end_date = Date.new(2008, 12, 13)
@@ -14,7 +17,7 @@ describe TeamNotesReport do
       StudentComment.should_receive(:find).and_return([sc1, sc2])
       Time.stub!(:now => Date.new(2008, 12, 12).to_time)
 
-      report_body = TeamNotesReport.render_text(:user => user, :start_date => start_date, :end_date => end_date)
+      report_body = TeamNotesReport.render_text(:user => user, :start_date => start_date, :end_date => end_date, :school => school)
 
       report_body.should == <<EOS
 Report Generated at December 12, 2008 00:00
