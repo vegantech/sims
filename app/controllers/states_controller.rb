fbeb373b957c297e9d2806c6626992605864015a
Shawn Balestracci <shawn@vegantech.com>
2009-02-08 20:14:19 -0600

    removed xml from states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,25 +1,21 @@
 class StatesController < ApplicationController
   additional_write_actions :reset_password, :recreate_admin
   # GET /states
-  # GET /states.xml
   def index
     @country = current_district.country
     @states = @country.states.normal
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @states }
     end
   end
 
   # GET /states/new
-  # GET /states/new.xml
   def new
     @state = State.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @state }
     end
   end
 
@@ -29,7 +25,6 @@ class StatesController < ApplicationController
   end
 
   # POST /states
-  # POST /states.xml
   def create
     @state = current_district.country.states.build(params[:state])
 
@@ -37,16 +32,13 @@ class StatesController < ApplicationController
       if @state.save
         flash[:notice] = 'State was successfully created.'
         format.html { redirect_to(states_url) }
-        format.xml  { render :xml => @state, :status => :created, :location => @state }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @state.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /states/1
-  # PUT /states/1.xml
   def update
     @state = current_district.state
     
@@ -55,16 +47,13 @@ class StatesController < ApplicationController
       if @state.update_attributes(params[:state])
         flash[:notice] = 'State was successfully updated.'
         format.html { redirect_to(root_url) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @state.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /states/1
-  # DELETE /states/1.xml
   def destroy
     @state = current_district.country.states.find(params[:id])
     @state.destroy
@@ -73,7 +62,6 @@ class StatesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(states_url) }
-      format.xml  { head :ok }
     end
   end
   
