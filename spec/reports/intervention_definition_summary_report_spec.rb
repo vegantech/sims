4ac8c07e70aad7e140d58ab8ee1eca49506fe388
Shawn Balestracci <shawn@vegantech.com>
2009-03-13 16:34:20 -0500

    fix report spec

--- spec/reports/intervention_definition_summary_report_spec.rb
+++ spec/reports/intervention_definition_summary_report_spec.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe InterventionDefinitionSummaryReport do
   describe 'render_text' do
     it 'should generate correct text output' do
-      # od = ObjectiveDefinition.create!(:title => 'Uppity', :description => 'Student does not know their place.')
       ic = Factory(:intervention_cluster, :title => 'IC-TITLE')
       id = Factory(:intervention_definition, :title => 'ID-TITLE', :intervention_cluster => ic)
       od = ic.objective_definition
@@ -15,13 +14,13 @@ describe InterventionDefinitionSummaryReport do
       report_body.should == <<EOS
 Report Generated at December 12, 2008 00:00
 
-1 - Some tier:
+ - Some tier:
 
-+-------------------------------------------------------------------------------------------------------------------------------------+
-|   Bus. Key    | Category |      Title      |     Description     | Duration / Frequency | Progress Monitors | Links and Attachments |
-+-------------------------------------------------------------------------------------------------------------------------------------+
-| 1-1-1    -1-1 | IC-TITLE | <b>ID-TITLE</b> | ID-TITLEDescription | 1 Default / 1 time   |                   |                       |
-+-------------------------------------------------------------------------------------------------------------------------------------+
++--------------------------------------------------------------------------------------------------------------------------------+
+| Bus. Key | Category |      Title      |     Description     | Duration / Frequency | Progress Monitors | Links and Attachments |
++--------------------------------------------------------------------------------------------------------------------------------+
+| -1-1-1-1 | IC-TITLE | <b>ID-TITLE</b> | ID-TITLEDescription | 1 Default / 1 time   |                   |                       |
++--------------------------------------------------------------------------------------------------------------------------------+
 
 EOS
     end
