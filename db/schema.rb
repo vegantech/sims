752695d4e0ba9eadee28e13c0668e0d556544c24
Shawn Balestracci <shawn@vegantech.com>
2009-03-27 11:40:41 -0500

    [#211 state:resolved] date_selects don't mix well with datetime fields when submitting, they get reset to nil or a default date.    Changed the probe administered_at from datetime to date.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090316004509) do
+ActiveRecord::Schema.define(:version => 20090327163254) do
 
   create_table "answer_definitions", :force => true do |t|
     t.integer  "element_definition_id"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.boolean  "autoset_others"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "answer_definitions", ["element_definition_id"], :name => "index_answer_definitions_on_element_definition_id"
@@ -61,6 +64,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.string   "document_content_type"
     t.integer  "document_file_size"
     t.datetime "document_updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "checklist_definitions", ["district_id"], :name => "index_checklist_definitions_on_district_id"
@@ -115,6 +121,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "element_definitions", ["question_definition_id"], :name => "index_element_definitions_on_question_definition_id"
@@ -170,6 +179,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.boolean  "disabled"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "goal_definitions", ["district_id"], :name => "index_goal_definitions_on_district_id"
@@ -199,6 +211,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.boolean  "disabled"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "intervention_clusters", ["objective_definition_id"], :name => "index_intervention_clusters_on_objective_definition_id"
@@ -231,6 +246,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.string   "rec_mon_preface"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "intervention_definitions", ["frequency_id"], :name => "index_intervention_definitions_on_frequency_id"
@@ -318,6 +336,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.boolean  "disabled"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "objective_definitions", ["goal_definition_id"], :name => "index_objective_definitions_on_goal_definition_id"
@@ -347,6 +368,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.string   "grade_level"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "probe_definition_benchmarks", ["benchmark"], :name => "index_probe_definition_benchmarks_on_benchmark"
@@ -364,6 +388,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "probe_definitions", ["district_id"], :name => "index_probe_definitions_on_district_id"
@@ -380,6 +407,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "second_digit"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "probe_questions", ["probe_definition_id"], :name => "index_probe_questions_on_probe_definition_id"
@@ -393,7 +423,7 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
   add_index "probe_questions_probes", ["probe_question_id"], :name => "index_probe_questions_probes_on_probe_question_id"
 
   create_table "probes", :force => true do |t|
-    t.datetime "administered_at"
+    t.date     "administered_at"
     t.integer  "score"
     t.integer  "district_id"
     t.integer  "intervention_probe_assignment_id"
@@ -410,6 +440,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "question_definitions", ["checklist_definition_id"], :name => "index_question_definitions_on_checklist_definition_id"
@@ -441,6 +474,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "recommendation_answer_definitions", ["recommendation_definition_id"], :name => "index_recommendation_answer_definitions_on_recommendation_definition_id"
@@ -464,6 +500,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "score_options"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "recommendation_definitions", ["checklist_definition_id"], :name => "index_recommendation_definitions_on_checklist_definition_id"
@@ -500,6 +539,9 @@ ActiveRecord::Schema.define(:version => 20090316004509) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
+    t.datetime "copied_at"
+    t.integer  "copied_from"
   end
 
   add_index "recommended_monitors", ["intervention_definition_id"], :name => "index_recommended_monitors_on_intervention_definition_id"
