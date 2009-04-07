37311a1e9e5edd4175f36e66fd47d294ce40a2f3
bendycode <stephen@bendycode.com>
2009-04-07 17:56:20 -0500

    [#213] Nicer headers.

--- app/reports/user_interventions_report.rb
+++ app/reports/user_interventions_report.rb
@@ -22,23 +22,17 @@ class UserInterventions
 
     table = @user.intervention_participants.report_table(:all,
       :only => [],
-      :methods => :role,
+      :methods => :role_title,
       :include => {:intervention => {:only => [:start_date, :end_date, :active],
                                      :methods => [:report_summary, :frequency_summary, :time_length_summary],
-                                     :include => {:student => {:only => [], :methods => [:fullname]}}
-                                     }
-                  }
-    )
+                                     :include => {:student => {:only => [], :methods => [:fullname]}}}})
 
-    # participants = @user.intervention_participants
-    # table = InterventionParticipant.report_table(:all, :only => ["role"], :conditions => ["intervention_participants.user_id = ?", @user.id])
+    return table if table.column_names.blank?
 
-    # return table if table.column_names.blank?
-
-    # table.rename_columns("role" => "Role", "intervention.report_summary" => "Title / Status",
-    #   "intervention.start_date" => "Start Date", "intervention.frequency_summary" => "Frequency",
-    #   "intervention.active" => "Active", "intervention.time_length_summary" => "Time Length",
-    #   "intervention.end_date" => "End Date")
+    table.rename_columns("role_title" => "Role", "intervention.report_summary" => "Title / Status",
+      "intervention.start_date" => "Start Date", "intervention.frequency_summary" => "Frequency",
+      "intervention.active" => "Active", "intervention.time_length_summary" => "Time Length",
+      "intervention.end_date" => "End Date")
 
     # table.reorder("Title / Status", "Role", "Start Date", "End Date", "Frequency", "Time Length", "Active", "intervention.student_name")
     table
