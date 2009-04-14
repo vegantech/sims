f67f344bc70e9f460e0716fa1023ddbb51f2c380
Shawn Balestracci <shawn@vegantech.com>
2009-04-14 12:20:28 -0500

    fix some specs for rails2.3.2

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require File.join(RAILS_ROOT, '/vendor/plugins/cells/boot')
 
 
 Rails::Initializer.run do |config|
