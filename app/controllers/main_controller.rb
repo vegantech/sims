80ca852dca7a201bfcc9916384194058b65e9821
Shawn Balestracci <shawn@vegantech.com>
2009-07-25 16:48:18 -0500

    autojump to school selection

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   skip_before_filter :authenticate, :authorize, :only=>:index
   include  CountryStateDistrict
   def index
-    if current_user and current_user.authorized_schools.present? and current_user.authorized_for?('schools','read')
+    if current_user and current_user.authorized_schools.present? and current_user.authorized_for?('schools','read_access')
       redirect_to schools_url and return
     end
     dropdowns
