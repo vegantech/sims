6eaee9c8e4efeb245f647314508ea5b01b04d145
Shawn Balestracci <shawn@vegantech.com>
2009-08-13 11:56:42 -0500

    intervention formatting changes 1-5

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -53,8 +53,8 @@ class Intervention < ActiveRecord::Base
   delegate :title, :tier, :description, :intervention_cluster, :to => :intervention_definition
   delegate :objective_definition, :to => :intervention_cluster
   delegate :goal_definition, :to => :objective_definition
-  named_scope :active, :conditions => {:active => true}
-  named_scope :inactive, :conditions => {:active => false}
+  named_scope :active, :conditions => {:active => true}, :order => 'created_at desc'
+  named_scope :inactive, :conditions => {:active => false}, :order => 'created_at desc'
 
   acts_as_reportable # if defined? Ruport
 
