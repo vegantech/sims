8f397cde48a83b71978100d14f3872ce46553b50
Shawn Balestracci <shawn@vegantech.com>
2009-11-05 17:22:15 -0600

    [#319 state:needs_tests] Intervention menu pdf/html regeneration deletes other files in directory now (renamed or deleted objectives).

--- lib/create_intervention_pdfs.rb
+++ lib/create_intervention_pdfs.rb
@@ -1,8 +1,11 @@
 require 'fileutils'
 class CreateInterventionPdfs
   def self.generate(district)
-    district.objective_definitions.each do |o| 
       dir = "#{RAILS_ROOT}/public/system/district_generated_docs/#{district.id}/"
+    old_files = Dir.glob("#{dir}*")
+    new_files = []
+    
+    district.objective_definitions.each do |o| 
       FileUtils.mkdir_p dir unless File.exists? dir
 
       File.open(dir+"#{o.title.split(" ").join("_")}.pdf",'w') do |f|
@@ -12,6 +15,12 @@ class CreateInterventionPdfs
       File.open(dir+"#{o.title.split(" ").join("_")}.html",'w') do |f|
         f << InterventionDefinitionSummaryReport.render_html(:objective_definition => o, :template => :standard)
       end
+
+      new_files << (dir+"#{o.title.split(" ").join("_")}.html")
+      new_files << (dir+"#{o.title.split(" ").join("_")}.pdf")
+
+      
     end
+     FileUtils.rm(old_files - new_files)
   end
 end
