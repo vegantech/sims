bffb1dfddcb9e92425bf34d16693c7c4b3eddae4
Shawn Balestracci <shawn@vegantech.com>
2009-11-23 12:05:39 -0600

    updated hoptoad

--- vendor/plugins/hoptoad_notifier/recipes/hoptoad.rb
+++ vendor/plugins/hoptoad_notifier/recipes/hoptoad.rb
@@ -6,14 +6,15 @@
 #
 # Defines deploy:notify_hoptoad which will send information about the deploy to Hoptoad.
 #
-after "deploy:cleanup", "deploy:notify_hoptoad"
+after "deploy",            "deploy:notify_hoptoad"
+after "deploy:migrations", "deploy:notify_hoptoad"
 
 namespace :deploy do
   desc "Notify Hoptoad of the deployment"
   task :notify_hoptoad do
-    rails_env = fetch(:rails_env, "production")
+    rails_env = fetch(:hoptoad_env, fetch(:rails_env, "production"))
     local_user = ENV['USER'] || ENV['USERNAME']
-    notify_command = "rake RAILS_ENV=#{rails_env} hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+    notify_command = "rake hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
     puts "Notifying Hoptoad of Deploy (#{notify_command})"
     `#{notify_command}`
     puts "Hoptoad Notification Complete."
