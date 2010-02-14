cb8a056f123e2e55a227ba09d80939ebf760136d
Shawn Balestracci <shawn@vegantech.com>
2010-02-14 08:12:56 -0800

    [#383 state:needs_tests] apply to all checkbox for interventions now adds comments and progress monitors to interventions

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -175,11 +175,14 @@ class Intervention < ActiveRecord::Base
     # make sure it doesn't create double interventions for the selected student
     # make sure it creates interventions for each student
     if self.apply_to_all == "1"
+      comment = {:comment => comments.present? ? comments.first.comment : nil}
       student_ids = self.selected_ids
       student_ids.delete(self.student_id.to_s)
+      ipa = @ipa.attributes || nil
       @interventions = student_ids.collect do |student_id|
         Intervention.create!(self.attributes.merge(:student_id => student_id, :apply_to_all => false,
-          :auto_implementer => self.auto_implementer, :called_internally => true, :participant_user_ids => self.participant_user_ids))
+          :auto_implementer => self.auto_implementer, :called_internally => true, :participant_user_ids => self.participant_user_ids,
+                                                  :comment => comment ,:intervention_probe_assignment => ipa))
       end
     end
     true
