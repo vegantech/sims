2d6f57b3f5218d7ddc2fdddca6702bb0c0f7c3e3
Shawn Balestracci <shawn@vegantech.com>
2010-02-23 10:46:27 -0800

    switching to subdomain_fu

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ server "sims.widpi.managedmachine.com", :app, :web, :db, :primary => true
 set :domain, 'sims.widpi.managedmachine.com'
 set :default_url, 'https://sims.widpi.managedmachine.com'
 
-after  :setup_domain_constant, :setup_default_url, :change_railmail_to_smtp, :setup_https_protocol
+after  :setup_domain_constant, :setup_default_url, :change_railmail_to_smtp, :setup_https_protocol, :enable_subdomains
 
--- config/deploy/veg-open.rb
+++ config/deploy/veg-open.rb
@@ -5,6 +5,7 @@ server "vegantech.com", :app, :web, :db, :primary => true
 set :login_note, 'This is the demo.   You use names like oneschool (look to the menu at the left for more.)'
 set :domain, "sims-open.vegantech.com"
 #  set :branch, 'aug-11-formatting-changes'
+ after  :setup_domain_constant, :enable_subdomains
 
 
 
