49a31ee052443666174b8b890a47f15b4260a47a
Shawn Balestracci <shawn@vegantech.com>
2010-02-24 21:19:09 -0800

    working on switching over to cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,25 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+#    can :manage, :all
+
+    can :news, :all do |item, parent|
+        if user.role?('news_admin') 
+          parent = System if parent.nil?
+          user.district == parent || user.district.try(:administers) == parent
+        end
+    end
+
+    can :read, School do |school|
+      (user.roles & ['regular_user','school_admin']).present? && user.authorized_schools.present?  
+    end
+
+    can :read, Student do |student|
+      user.role?('regular_user')
+    end
+
+  end
+
+end
---
+++ db/migrate/20100225000140_convert_roles_to_roles_mask.rb
@@ -0,0 +1,32 @@
+class ConvertRolesToRolesMask < ActiveRecord::Migration
+  def self.up
+    User.all.each do |u|
+      u.roles = u.old_roles.collect(&:name)
+      u.update_attribute(:roles_mask,u.roles_mask)
+    end
+  end
+
+  def self.down
+    db_roles = Role.all.group_by(&:name)
+    User.all.each do |u|
+      u.old_roles=db_roles.collect{|k,v| v if  u.roles.include?(k)}.compact.flatten
+    end
+  end
+
+  class User < ActiveRecord::Base
+    ROLES = ["district_admin", "content_admin", "school_admin", "regular_user", "news_admin", "state_admin", "country_admin"]
+    has_and_belongs_to_many :old_roles, :join_table => 'roles_users', :class_name => 'Role'
+    has_many :rights, :through => :old_roles
+
+    def roles=(roles)
+      self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+    end
+
+    def roles
+      ROLES.reject do |r|
+        ((roles_mask || 0) & 2**ROLES.index(r)).zero?
+      end
+    end
+
+  end
+end
