e79e90d834d9fe0649f85f981ee2a4e48ff80144
Shawn Balestracci <shawn@vegantech.com>
2010-02-28 07:23:32 -0800

    fix for staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,6 @@ set :rails_env, "staging"
 
 task :setup_domain_constant do
     run "cd #{release_path}/config/initializers && sed -i  -e 's/#SIMS_DOMAIN =/SIMS_DOMAIN =\"staging.sims.widpi.managedmachine.com\"/' host_info.rb "
+    run "cd #{release_path}/config/initializers && sed -i  -e 's/www///' subdomain_fu.rb "
 end
 
