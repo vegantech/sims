4e64aa1e44f810ea99850bdc26cbdaeda0112441
Shawn Balestracci <shawn@vegantech.com>
2010-03-30 03:45:51 +0000

    remove a debugging raise

--- app/models/checklist_definition.rb
+++ app/models/checklist_definition.rb
@@ -82,7 +82,6 @@ class ChecklistDefinition < ActiveRecord::Base
   protected
 
   def before_save
-    raise 'why am I called'
     if active?
       district.checklist_definitions.active_checklist_definition.update_attribute(:active, false)
     end
