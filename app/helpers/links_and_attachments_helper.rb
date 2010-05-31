07e46a981d735565d5e237335d9eda9465491d55
Shawn Balestracci <shawn@vegantech.com>
2010-05-31 00:41:59 +0000

    [#445 state:needs_tests] display images inline for attached files

--- app/helpers/links_and_attachments_helper.rb
+++ app/helpers/links_and_attachments_helper.rb
@@ -15,7 +15,13 @@ module LinksAndAttachmentsHelper
     return "" unless obj.respond_to? "assets"
     obj.assets.inject("") do |str,asset|
       str += content_tag(tag_type, link_to_with_icon(asset.name, asset.url)) unless asset.url.blank? 
-      str += content_tag(tag_type, link_to_with_icon(asset.document.original_filename, asset.document.url)) if asset.document? 
+      if asset.document?
+        if ["image/jpeg", "image/png", "image/gif"].include?(asset.document.content_type.strip)
+          str += content_tag(tag_type, image_tag(asset.document.url, :alt=>asset.document.original_filename)) if asset.document? 
+        else
+          str += content_tag(tag_type, link_to_with_icon(asset.document.original_filename, asset.document.url)) if asset.document? 
+        end
+      end
       str
     end
 
