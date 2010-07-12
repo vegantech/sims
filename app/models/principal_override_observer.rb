9285a0837658db586ca5c7fcc37275a4d55d549f
Shawn Balestracci <shawn@vegantech.com>
2010-07-12 04:18:23 +0000

    [#470 state:needs_tests] principal responses for users that no longer exist

--- app/models/principal_override_observer.rb
+++ app/models/principal_override_observer.rb
@@ -4,7 +4,7 @@ class PrincipalOverrideObserver < ActiveRecord::Observer
   end
 
   def after_update(principal_override)
-    Notifications.deliver_principal_override_response(principal_override) if principal_override.send_email
+    Notifications.deliver_principal_override_response(principal_override) if principal_override.send_email and principal_override.teacher.present?
   end
 
 
