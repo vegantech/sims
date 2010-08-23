ef55450491e9f464c9651c80cb206bad1aa9b1bc
Shawn Balestracci <shawn@vegantech.com>
2010-08-23 17:50:10 +0000

    [#366] setup lib spec

--- features/step_definitions/scripted.rb
+++ features/step_definitions/scripted.rb
@@ -17,18 +17,8 @@ Given /^a student with district_student_id "([^"]*)"$/ do |id|
 end
 
 Given /^an intervention_definition with id "([^"]*)"$/ do |id|
+  InterventionDefinition.delete_all
   int_def = Factory(:intervention_definition, :title => 'cuke1', :description => 'cuke1' )
   InterventionDefinition.update_all("id = #{id}", "id = #{int_def.id}")
   GoalDefinition.update_all("district_id = #{@user.district_id}")
 end
-
-
-
-When /^I upload the automated_intervention sample file using curl$/ do
-  system 'script/server -p39001 -e cucumber &'
-  sleep 10 #allow test server to run
-  `curl --user automated_intervention:e  -Fupload_file=@./test/csv/automated_intervention/sample.csv http://127.0.0.1:39001/scripted/automated_intervention?district_abbrev=#{@user.district.abbrev}`
-  system "kill `ps aux | grep script/server | grep -e '-p 39001' | grep -v grep | awk '{ print $2 }'`"
-
-end
-
--- spec/lib/automated_intervention_spec.rb
+++ spec/lib/automated_intervention_spec.rb
@@ -1,5 +1,56 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AutomatedIntervention do
+  it 'should report an invalid file' do
+    importer=AutomatedIntervention.new File.new('test'),User.new
+    importer.import.to_s.should =~ /Invalid headers: They must be #{AutomatedIntervention::FORMAT}/
+  end
+
+  it 'should report a file with incorrect headers' do
+    importer=AutomatedIntervention.new File.new('README'),User.new
+    importer.import.to_s.should =~ /Invalid headers: They must be #{AutomatedIntervention::FORMAT}/
+  end
+
+  describe 'successful upload' do
+    before(:all) do
+      @user=Factory(:user)
+      stu=Factory(:student,:district_id => @user.district_id).update_attribute(:district_student_id,'cuke123')
+      InterventionDefinition.delete_all
+      int_def = Factory(:intervention_definition, :title => 'cuke1', :description => 'cuke1' )
+      InterventionDefinition.update_all("id = 99876", "id = #{int_def.id}")
+      GoalDefinition.update_all("district_id = #{@user.district_id}")
+      @importer=AutomatedIntervention.new File.new('test/csv/automated_intervention/sample.csv'), @user
+      @importer.import
+
+    end
+    it 'should return the proper messages' do
+      expected_messages = 
+      [
+          "Processing file", "2 interventions added",
+          "Unknown student with district_student_id invalid123",
+          "Invalid Intervention Definition ID -1",
+          "Duplicate entry for #<FasterCSV::Row district_student_id:\"cuke123\" intervention_definition_id:\"99876\" start_date:\"2008-01-02\" comment:\"This is a duplicate intervention that should not be added.\">"
+      ].to_set
+      @importer.messages.to_set.should == expected_messages
+
+
+    end
+
+    it 'should have created the first intervention in the file'  do
+      InterventionComment.find_by_comment("This is a comment").intervention.end_date.to_s.should == '2008-01-01'
+
+    end
+
+    it 'should have created the second intervention in the file' do
+      InterventionComment.find_by_comment("This is another intervention on a different date").intervention.end_date.to_s.should == '2008-01-02'
+    end
+  end
+
+
+  #3 cuke123,99876,2008-01-01,"This is a comment"
+  #4 cuke123,99876,2008-01-02,"This is another intervention on a different date"
+  #5 cuke123,99876,2008-01-02,"This is a duplicate intervention that should not be added."
+
+
 end
 
