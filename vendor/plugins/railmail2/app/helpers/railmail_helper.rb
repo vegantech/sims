ff0f51e72c77ddce9d191adf9fa8eb2550b603cb
Shawn Balestracci <shawn@vegantech.com>
2010-09-11 03:27:44 +0000

    [#502] starting first html email, starting with the special ed referral for testing, had to make a change to railmail as well

--- vendor/plugins/railmail2/app/helpers/railmail_helper.rb
+++ vendor/plugins/railmail2/app/helpers/railmail_helper.rb
@@ -4,6 +4,7 @@ module RailmailHelper
     raw = d.raw
     return raw.body unless raw.multipart?
     
+    return raw.quoted_body
     part = raw.parts.select {|p| p.content_type == 'text/html'}.first.body
     part ||= "<pre class=\"plain_text\">#{raw.parts.select {|p| p.content_type == 'text/plain'}.first.body}</pre>"
     part ||= raw.parts[0].body
