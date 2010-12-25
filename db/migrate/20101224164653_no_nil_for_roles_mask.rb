7e32081142e34ddb3c2dee2561a735b239221c99
Shawn Balestracci <shawn@vegantech.com>
2010-12-25 20:15:56 +0000

    roles mask should not allow null migration

---
+++ db/migrate/20101224164653_no_nil_for_roles_mask.rb
@@ -0,0 +1,9 @@
+class NoNilForRolesMask < ActiveRecord::Migration
+  def self.up
+    change_column :users, :roles_mask, :integer, :null => false, :default => 0
+  end
+
+  def self.down
+    change_column :users, :roles_mask, :integer, :null => true, :default => 0
+  end
+end
