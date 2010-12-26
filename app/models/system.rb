e1039a0c01a769cc8f805ab04027b653f3d7a0b7
Shawn Balestracci <shawn@vegantech.com>
2010-12-26 22:20:28 +0000

    merge moonshine upgrade branch, remove cap branch for staging (it should now be master) add more disallowed distric abbrevs

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,5 +1,5 @@
 class System
-  RESERVED_SUBDOMAINS = %w{ www asset demo asset2 asset1 asset0 mail staging }
+  RESERVED_SUBDOMAINS = %w{ www asset demo asset2 asset1 asset0 mail staging assets }
   HASH_KEY = ''
   
   def self.news
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,5 @@ set :default_url, 'https://www.staging.simspilot.org'
 server "staging.sims.widpi.managedmachine.com", :app, :web, :db, :primary => true
 set :domain, 'staging.simspilot.org'
 set :rails_env, "staging"
-set :branch, "moonshine_upgrade"
 
 after  :setup_domain_constant, :setup_default_url,  :setup_https_protocol, :enable_subdomains
