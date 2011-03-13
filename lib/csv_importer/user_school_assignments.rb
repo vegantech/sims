7cab4c9c1961f3efe0aa262d3507cc6059a50129
Shawn Balestracci <shawn@vegantech.com>
2011-03-13 05:06:56 +0000

    [#505] test user_schools_assignments

--- lib/csv_importer/user_school_assignments.rb
+++ lib/csv_importer/user_school_assignments.rb
@@ -56,7 +56,7 @@ module CSVImporter
 
     def migration t
       t.column :district_school_id, :integer
-      t.column :district_user_id, :string
+      t.column :district_user_id, :string, :limit => User.columns_hash["district_user_id"].limit, :null => User.columns_hash["district_user_id"].null
     end
 
     def delete
@@ -64,19 +64,23 @@ module CSVImporter
        delete from usa using  user_school_assignments usa 
        inner join schools sch on usa.school_id = sch.id and sch.district_id= #{@district.id}
        inner join users u on usa.user_id = u.id
-       where sch.district_school_id != ''  and u.district_user_id != ''
-        and (usa.admin = false or usa.admin is null)
+       left outer join #{temporary_table_name} tusa 
+       on tusa.district_school_id = sch.district_school_id and 
+       tusa.district_user_id = u.district_user_id
+       where sch.district_school_id is not null  and u.district_user_id != ''
+        and (usa.admin = false or usa.admin is null) and tusa.district_school_id is null
         "
       ActiveRecord::Base.connection.update query
     end
 
     def insert
-      query=("insert into user_school_assignments
+      query=("insert into user_school_assignments 
       (school_id, user_id , created_at, updated_at)
       select sch.id, u.id,  curdate(), curdate() from #{temporary_table_name} tusa
       inner join schools sch on sch.district_school_id = tusa.district_school_id
       inner join users u on u.district_user_id = tusa.district_user_id
-      where sch.district_id= #{@district.id} and u.district_id = #{@district.id}
+      left outer join user_school_assignments usa on usa.school_id = sch.id and usa.user_id = u.id
+      where sch.district_id= #{@district.id} and u.district_id = #{@district.id} and usa.id is null
       "
       )
       ActiveRecord::Base.connection.update query
---
+++ spec/lib/csv_importer/user_school_assignments_spec.rb
@@ -0,0 +1,71 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe CSVImporter::AllStudentsInSchools do
+  describe "importer"  do
+    it 'should work properly' do
+      #be sure to test with existing admin
+      School.delete_all
+      UserSchoolAssignment.delete_all
+      District.delete_all
+      User.delete_all
+
+      @no_role_or_district_user_id = Factory(:user)
+      @district = @no_role_or_district_user_id.district
+      @school_no_link = Factory(:school, :district_id => @district.id)
+      @school_with_link = Factory(:school, :district_id => @district.id, :district_school_id => 'linked_school')
+      @school_with_link_admin = Factory(:user, :district_id => @district.id, :district_user_id => 'school_with_link_admin')
+      @school_with_link_admin.user_school_assignments.create!(:school_id => @school_with_link.id, :admin => true) 
+      @school_with_link_admin2 = Factory(:user, :district_id => @district.id, :district_user_id => 'school_with_link_admin2')
+      @school_with_link_admin2.user_school_assignments.create!(:school_id => @school_with_link.id, :admin => true) 
+      
+      @no_role_or_district_user_id.user_school_assignments.create!(:school_id => @school_no_link.id)
+      
+      
+      @role_no_district_user_id = Factory(:user,:district_id => @district.id)
+      @should_lose_role = Factory(:user,:district_id => @district.id,  :district_user_id => 'should_lose_role')
+      @should_keep_role = Factory(:user,:district_id => @district.id,  :district_user_id => 'should_keep_role')
+      @should_keep_no_role = Factory(:user,:district_id => @district.id, :district_user_id => 'should_keep_no_role')
+      @should_gain_role = Factory(:user,:district_id => @district.id, :district_user_id => 'should_gain_role')
+      @dup_person_id = Factory(:user,:district_id => @district.id, :district_user_id => @should_gain_role.district_user_id)
+      @dup_person_id.user_school_assignments.create!(:school_id => @school_no_link.id)
+      [@role_no_district_user_id, @should_lose_role, @should_keep_role].each do |u|
+        u.user_school_assignments.create!(:school_id => @school_with_link.id)
+      end
+
+
+      @role_no_district_user_id.user_school_assignments.create!(:school_id => @school_no_link.id)
+      @district.users.update_all("updated_at = '2000-01-01'")
+      @i=CSVImporter::UserSchoolAssignments.new "#{Rails.root}/spec/csv/user_school_assignments.csv",@district
+      @i.import
+
+      @school_with_link_admin.reload.user_school_assignments.collect(&:admin).should == [true]
+      @school_with_link_admin2.reload.user_school_assignments.collect(&:admin).should == [true]
+
+
+      @no_role_or_district_user_id.reload.user_school_assignments.size.should == 1
+      @no_role_or_district_user_id.user_school_assignments.find_all_by_school_id(@school_no_link.id).size.should == 1
+
+      @should_lose_role.reload.user_school_assignments == []
+      @should_keep_no_role.user_school_assignments.should == []
+
+      [@should_keep_role,@should_gain_role].each{|u| check_user_school_assignments u}
+      @dup_person_id.reload.user_school_assignments.count.should == 2
+
+      @dup_person_id.school_ids.to_set.should == [@school_no_link.id, @school_with_link.id].to_set
+      @dup_person_id.user_school_assignments.collect(&:school_id).to_set.should == [@school_no_link.id, @school_with_link.id].to_set
+      
+      @role_no_district_user_id.school_ids.to_set.should == [@school_no_link.id, @school_with_link.id].to_set
+      @role_no_district_user_id.user_school_assignments.collect(&:school_id).to_set.should == [@school_no_link.id, @school_with_link.id].to_set
+    end
+
+    def check_user_school_assignments u
+      u.reload.user_school_assignments.size.should == 1
+      u.user_school_assignments.find_all_by_school_id(@school_with_link).size.should == 1
+    end
+
+  end
+end
+
+
+
+
