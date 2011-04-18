d089bcab1076e0392300a3afccb232ba1ce479bc
Shawn Balestracci <shawn@vegantech.com>
2011-04-18 02:25:36 +0000

    [#632] add blue coloring to student and user interventions report.   Reports are done.

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -163,6 +163,10 @@ class Intervention < ActiveRecord::Base
     intervention_probe_assignments.active.collect(&:title).join(";")
   end
 
+  def bolded_report_summary
+    "#{intervention_definition.bolded_title}  #{'Ended: ' + ended_at.to_s(:chatty) unless active}"
+  end
+
   def report_summary
     "#{title} #{'Ended: ' + ended_at.to_s(:chatty) unless active}"
   end
--- app/reports/user_interventions_report.rb
+++ app/reports/user_interventions_report.rb
@@ -9,6 +9,19 @@ class UserInterventionsReport < DefaultReport
     self.data = UserInterventions.new(options)
   end
 
+  class HTML < Ruport::Formatter::HTML
+    renders :html, :for => UserInterventionsReport
+    build :header do
+      output << "Report Generated at #{Time.now.to_s(:long)}"
+    end
+
+    build :body do
+      output << data.to_grouping(format=:html).to_html.gsub('<table>', '<table class="user_interventions_table">')
+    end
+  end
+
+
+
   class PDF < Ruport::Formatter::PDF
     renders :pdf, :for => UserInterventionsReport
     build :header do
@@ -16,9 +29,11 @@ class UserInterventionsReport < DefaultReport
     end
 
     build :body do
+      ::PDF::Writer::TagAlink.style={:factor=>0.05, :text_color=>Color::RGB::Blue, :draw_line=>false, :line_style=>{:dash=>{:phase=>0, :pattern=>[]}}, :color=>Color::RGB::Blue}
       pdf_writer.start_page_numbering(350,10,8,:center,"Page: <PAGENUM>")
 
-      output << render_grouping(data.to_grouping, options.to_hash.merge(:formatter=> pdf_writer))
+      output << render_grouping(data.to_grouping(:pdf), options.to_hash.merge(:formatter=> pdf_writer))
+      ::PDF::Writer::TagAlink.style=nil
     end
   end
 end
@@ -30,19 +45,24 @@ class UserInterventions
     @user = options[:user]
   end
 
-  def to_table
+  def to_table(format=nil)
     return unless defined? Ruport
+    if [:pdf,:html].include?format
+      title_method = :bolded_report_summary
+    else
+      title_method = :report_summary
+    end
 
     table = @user.intervention_participants.report_table(:all,
       :only => [],
       :methods => :role_title,
       :include => {:intervention => {:only => [:start_date, :end_date, :active],
-                                     :methods => [:report_summary, :frequency_summary, :time_length_summary],
+                                     :methods => [title_method, :frequency_summary, :time_length_summary],
                                      :include => {:student => {:only => [], :methods => [:fullname]}}}})
 
     return table if table.column_names.blank?
 
-    table.rename_columns("role_title" => "Role", "intervention.report_summary" => "Title / Status",
+    table.rename_columns("role_title" => "Role", "intervention.#{title_method}" => "Title / Status",
       "intervention.start_date" => "Start Date", "intervention.frequency_summary" => "Frequency",
       "intervention.active" => "Active", "intervention.time_length_summary" => "Time Length",
       "intervention.end_date" => "End Date", 'student.fullname' => 'Student Name')
@@ -51,8 +71,8 @@ class UserInterventions
     table
   end
 
-  def to_grouping
-    @table ||= to_table
+  def to_grouping(format=nil)
+    @table ||= to_table(format)
     return @table if @table.column_names.blank?
     Ruport::Data::Grouping(@table, :by => "Student Name")
   end
