f6a63d9f9ef10612ab53a574497e4f20a10febb7
Shawn Balestracci <shawn@vegantech.com>
2011-05-08 22:37:35 +0000

    doing quick mockup of checkin checkout form

---
+++ lib/cico_test.rb
@@ -0,0 +1,8 @@
+class CicoTest
+  STUDENTS=['Student 1', 'Student 2', 'Student N']
+  EXPECTATIONS=['Expectation 1', 'Expectation 2', 'Expectation N']
+  PERIODS=['Period 1', 'Period 2', 'Period N']
+  class Dog
+  end
+
+end
