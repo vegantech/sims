c5582286bb45eece185be7ff905fd6e78318b07c
Shawn Balestracci <shawn@vegantech.com>
2011-06-05 22:04:49 +0000

    [#642] ruby library to connect to demo student district verification

---
+++ lib/verify_student_in_district_externally.rb
@@ -0,0 +1,54 @@
+module VerifyStudentInDistrictExternally
+  require 'net/https'
+  require 'uri'
+  require 'nokogiri'
+
+  def self.verify(student,district)
+    if true 
+      verify_at_dpi(student,district)
+    else
+      raise "Unsupported"
+    end
+
+  end
+
+#true return true
+#false return false
+#error throw exception
+
+  def self.verify_at_dpi(student,district)
+#   curl "https://uaapps.dpi.wi.gov/SIMS_Student_Location_Confirm/SIMS/nonsecure"
+ #  -d wsn=9000000099 -d district=3456 -H "Accept: text/xml"
+
+
+   uri = URI.parse("https://uaapps.dpi.wi.gov/SIMS_Student_Location_Confirm/SIMS/nonsecure")
+   http=Net::HTTP.new(uri.host, uri.port)
+   http.use_ssl=true
+   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+   request = Net::HTTP::Post.new(uri.request_uri)
+   request.set_form_data({"district" => "#{district}", "wsn"=>"#{student}"})
+   request["Accept"]="text/xml"
+   #set timeout here
+   response=http.request(request)
+   #throw if timeout
+   parsed_response=Nokogiri.parse(response.body)
+
+   if parsed_response.css('error').first.content == "false"
+     return parsed_response.css('found').first.content == "true"
+   else
+     #throw parsed_response.css('errorString').first.content
+   end
+
+   #timeout
+   #check for error and error string
+   #if error throw error string
+   #return found
+
+
+
+
+  end
+
+
+end
