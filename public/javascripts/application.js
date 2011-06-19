427c80dd89934c09c14d0b9568e71f33de05f66d
Shawn Balestracci <shawn@vegantech.com>
2011-06-19 19:01:52 +0000

    [#620] add period and expectation subtotals

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,7 +10,7 @@
 function school_day_change() {
   v=$('cico_school_day_status').value;
   if (v=='In School') {
-  $$('#cico_form select').each(function(selbox) {
+  $$('#cico_form select.student_day').each(function(selbox) {
       selbox.value='No Data';
       selbox.enable();
 
@@ -88,22 +88,53 @@ function update_cico_totals() {
   $$('#cico_form  table.student_day').each(function(student_day){
     count=0;
     total=0;
+    expectation_totals = new Array();
+    period_totals = new Array();
       student_day.select(".period_expectation_value select").each(function(score){
-        score_value=score.options[score.selectedIndex].value
-        v=parseInt(score_value)
+        pindex=score.classList[0].split('_')[2];
+        eindex=score.classList[0].split('_')[3];
+        score_value=score.options[score.selectedIndex].value;
+        v=parseInt(score_value);
         if (score_value == 'No Data') {v=0};
         if (v>=0) {
           count=count+1;
           total=total+v;
+          if (isNaN(period_totals[pindex])) {
+          period_totals[pindex] = v;
           }
+          else
+          {
+          period_totals[pindex] += v;
+          };
 
-        });
+          if (isNaN(expectation_totals[eindex])) {
+          expectation_totals[eindex] = v;
+          }
+          else
+          {
+            expectation_totals[eindex] += v;
+          };
+
+        }
+
+      });
       percent = Math.round(100*(100*(total/(count * parseInt($('cico_max_score').value)) )),2)/100;
       if (isNaN(percent)) {percent = ''}
       else {percent = percent + '%'};
       student_day.down(".student_day_total span.total_value").innerHTML=percent;
+      period_totals.each(function(p,pi){
+          student_day.down(".period_"+pi).innerHTML=p;
+          });
 
+      expectation_totals.each(function(e,ei){
+          student_day.down(".expectation_"+ei).innerHTML=e;
+          
+          });
       });
+
+
+
+
 }
 
 
