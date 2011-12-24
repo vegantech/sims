7b5f5a4a6bc80cd29a21db31d38765452c3401e6
Shawn Balestracci <shawn@vegantech.com>
2011-12-24 03:50:42 +0000

    fix changes to Rails.rot

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -2,7 +2,7 @@ class FileController < ApplicationController
   skip_before_filter :authorize, :only=>'download'
   def download
     unless params['filename'].include?('..')
-      send_file(File.join(Rails.root,'file',params['filename']), :x_sendfile=>true) 
+      send_file(File.join(Rails.root,'file',params['filename']), :x_sendfile=>true)
     else
       redirect_to root_url, :status=>403
     end
