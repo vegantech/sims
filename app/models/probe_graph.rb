561ec6a79263b604bdedcede832865572e9a1fb8
Shawn Balestracci <shawn@vegantech.com>
2012-01-08 06:24:10 +0000

    [#631] refactored graph, includes mmsd changes when I need to test, will support it for them tomorrow

--- app/models/probe_graph.rb
+++ app/models/probe_graph.rb
@@ -11,6 +11,7 @@ class ProbeGraph
     @district = opts[:district]
     @first_date = opts[:first_date]
     @end_date = opts[:end_date]
+    @chxp = []
   end
 
 
@@ -27,23 +28,39 @@ class ProbeGraph
   end
 
   def line_chart
-    singleton_class.send :include, ProbeGraph::WithAimLine
-    old_line_chart
-  end
-
-  def old_line_chart
-    #remove this when the mmsd chart is incorporated
     google_line_chart
   end
 
-  def new_line_chart
-    #mmsd only initially
+  def google_line_chart_mmsd
+    return ''if probes_for_graph.empty?
+
+    if probes_for_graph.count >= 50
+     custom_chm="chm=" + [max_min_zero,dots_for_line_graph,benchmark_lines].compact.join("|")
+    else
+     custom_chm=[numbers_on_line,max_min_zero,dots_for_line_graph,benchmark_lines].compact.join("|")
+    end
+    custom_string = [custom_chm,chxp].compact.join("&")
+
+    Gchart.line_xy({:data => line_graph_data(probes_for_graph),
+                   :axis_with_labels => 'x,x,y,r',
+                   :axis_labels => line_axis_labels,
+                   :bar_colors => "8DACD0,99DD99",
+                   :format=>'image_tag',
+                   :encoding => 'text',
+                   :custom => custom_string,
+                   :bar_width_and_spacing => nil,
+                   :size => '800x250',
+                   :thickness => "2|1,4,2", #thickness of scores | aim line with dashing
+                   :chds => chds,
+                   :axis_range => [[0,line_graph_date_denom],[],[0,100],[0,100]]
+    })
   end
+
+
 
   def google_line_chart
     #groups of 10, repeats the previous point on the next graph as a line graph needs at least 2 points
    return ''if probes_for_graph.empty?
-    @chxp=[]
     group=0
     probes_for_graph.in_groups_of(10,false).collect{ |probes_for_this_graph|
       custom_chm=[numbers_on_line,max_min_zero,dots_for_line_graph,benchmark_lines].compact.join("|")
@@ -56,10 +73,9 @@ class ProbeGraph
       else
       end
 
-      custom_string = [custom_chm,chart_margins,chxp].compact.join("&")
-    
+      custom_string = [custom_chm,chxp].compact.join("&")
       Gchart.line({:data => probes_for_this_graph.collect(&:score), :axis_with_labels => 'x,x,y,r',
-                 :axis_labels => axis_labels(probes_for_this_graph), 
+                 :axis_labels => axis_labels(probes_for_this_graph),
                  :bar_width_and_spacing => '30,25',
                  :bar_colors => probes_for_this_graph.collect{|e| (e.score<0)? '8DACD0': '5A799D'}.join("|"),
                  :format=>'image_tag',
@@ -67,7 +83,6 @@ class ProbeGraph
                  :encoding => 'text',
                  :custom => custom_string,
                  :size => '600x250'
-
                  })}.join("<br />")
 
 
@@ -81,9 +96,8 @@ class ProbeGraph
  def google_bar_chart
    #groups of 10
    return ''if probes_for_graph.empty?
-    @chxp=[]
     custom_chm=[numbers_in_bars,max_min_zero,benchmark_lines].compact.join("|")
-    custom_string = [custom_chm,chart_margins,chxp].compact.join("&")
+    custom_string = [custom_chm,chxp].compact.join("&")
     probes_for_graph.in_groups_of(10,false).collect{|probes_for_this_graph|
       Gchart.bar(:data => probes_for_this_graph.collect(&:score), :axis_with_labels => 'x,x,y,r',
                  :axis_labels => axis_labels(probes_for_this_graph),
@@ -94,31 +108,47 @@ class ProbeGraph
                  :encoding => 'text',
                  :custom => custom_string,
                  :size => '600x250'
-
                 )}.join("<br />")
   end
 
   def probes_for_graph
-    @probes.reject{|e| e.administered_at.blank? || e.score.blank?}.sort_by(&:administered_at)
+    @pfg ||= @probes.reject{|e| e.administered_at.blank? || e.score.blank?}.sort_by(&:administered_at)
   end
 
   def benchmarks_with_goal
-    @benchmarks |[goal_benchmark]
+    (@benchmarks |[goal_benchmark]).compact
   end
 
   def goal_benchmark
-      ProbeDefinitionBenchmark.new(:benchmark=>@goal, :grade_level => '   Goal') if @goal
+    ProbeDefinitionBenchmark.new(:benchmark=>@goal, :grade_level => '   Goal') if @goal.present?
   end
 
   protected
 
+ def y_axis_labels
+   if max == min
+     vals = [max]
+   else
+     diff = max-min
+     step = (diff/10.0).ceil
+     vals = ((min..max).step(step).to_a | [min,0,max]).sort.uniq
+   end
+   unless @y_in_chxp
+     to_chxp = "2,#{vals.collect{|v| scale_value(v)*100}.join(",")}"
+     @chxp << to_chxp
+     @y_in_chxp = true
+   end
+   vals
+ end
+
+
   def axis_labels(p_for_this_graph)
       [
-        p_for_this_graph.collect{|p| p.administered_at.to_s(:report)}, 
-        p_for_this_graph.collect(&:score), 
-        [min,0,max],
+        p_for_this_graph.collect{|p| p.administered_at.to_s(:report)},
+        p_for_this_graph.collect(&:score),
+        y_axis_labels,
         benchmarks_with_goal.collect{|b| "#{b.benchmark}-  #{b.grade_level}"},
-      ] 
+      ]
   end
 
   def dots_for_line_graph
@@ -136,7 +166,6 @@ class ProbeGraph
 
   def max_min_zero
     #min, zero, max
-    @chxp<<"2,#{scale_value(min)*100},#{scale_value(0)*100},#{scale_value(max)*100}"
     "r,000000,0,0.0,0.002|r,000000,0,#{scale_value(0) - 0.001},#{scale_value(0) + 0.001}|r,000000,0,0.998,1.0"
   end
 
@@ -147,12 +176,6 @@ class ProbeGraph
     end
   end
 
-  def chart_margins
-    "chma=20,20,20,20"
-    ""
-  end
-
-  
   def scale_value(value)
     (value-min).to_f/(max-min).to_f
   end
@@ -165,111 +188,59 @@ class ProbeGraph
     s=scores
     s << @goal.to_i if @goal
     s
-    
   end
 
   def min
-     @probe_definition.minimum_score || (scores_with_goal.min >=0 ? 0 : scores_with_goal.min)
+     @min ||= @probe_definition.minimum_score || (scores_with_goal.min >=0 ? 0 : scores_with_goal.min)
   end
 
   def max
-     @probe_definition.maximum_score || (scores_with_goal.max <= 10 ? 10 : scores_with_goal.max)
+     @max ||= @probe_definition.maximum_score || (scores_with_goal.max <= 10 ? 10 : scores_with_goal.max)
   end
 
-  module WithAimLine
-    def google_line_chart
-      #groups of 10, repeats the previous point on the next graph as a line graph needs at least 2 points
-      return ''if probes_for_graph.empty?
-      @chxp=[]
-      group=0
-      probes_for_graph.in_groups_of(10000,false).collect{ |probes_for_this_graph|
-        custom_chm=[numbers_on_line,max_min_zero,dots_for_line_graph,benchmark_lines].compact.join("|")
-        if group>0
-          probes_for_this_graph= [probes_for_graph[group*10-1]] + probes_for_this_graph
-        end
-        group+=1
-        if probes_for_this_graph.size == 1
-          probes_for_this_graph[1] = probes_for_this_graph[0]
-          #        custom_chm << "|@o,000000,0,0:#{scale_value(probes_for_this_graph[0].score)},4"
-        end
+  def chds
+    #min am nax for data series
+    a=[0,line_graph_date_denom,min,max]
+    a *= 2 if @goal #aim line and data have same range
+    a.join(",")
+  end
 
-        custom_string = [custom_chm,chart_margins,chxp,chls,line_chds].compact.join("&")
+  def line_axis_labels
+    [
+      [line_graph_left_date, line_graph_left_date + line_graph_date_denom/4 , line_graph_left_date + line_graph_date_denom/2, line_graph_left_date + 0.75 * line_graph_date_denom, line_graph_right_date].uniq,
+      [],
+      y_axis_labels,
+      benchmarks_with_goal.collect{|b| "#{b.benchmark}-  #{b.grade_level}"},
+    ]
+  end
 
-        Gchart.line_xy({:data => line_graph_data(probes_for_this_graph),
-                       :axis_with_labels => 'x,x,y,r',
-                       :axis_labels => line_axis_labels,
-                       :bar_colors => "8DACD0,99DD99",
-                       :format=>'image_tag',
-                       :encoding => 'text',
-                       :custom => custom_string,
-                       :bar_width_and_spacing => nil,
-                       :size => '600x250',
-                       :chds => nil,
-                       :axis_range => [[0,line_graph_date_denom],[],[0,100],[0,100]]
-        })}.join("<br />")
-
-
-
+  def line_graph_data(probes_for_this_graph)
+    a=[]
+    lg_dates=scaled_dates(probes_for_this_graph)
+    a << lg_dates
+    a << probes_for_this_graph.collect(&:score)
+    if @goal
+      a << [lg_dates.first, (@end_date - line_graph_left_date).to_i]
+      a << [probes_for_this_graph.first.score, @goal]
     end
+    a
+  end
 
-    def chls
-      "chls=2|1,4,2"
-    end
+  #TODO
+  def line_graph_left_date
+    (probes_for_graph.collect(&:administered_at) |[@first_date]).min
+  end
 
-    def line_chds
-      "chds=#{chds}"
-    end
+  def line_graph_right_date
+    (probes_for_graph.collect(&:administered_at) |[@end_date]).max
+  end
 
-    def chds
-      a=[0,line_graph_date_denom,min,max].join(",")
-      if @goal
-        [a,a].join(",")
-      else
-        a
-      end
-    end
-
-    protected
-
-    def line_axis_labels
-      [
-        [line_graph_left_date, line_graph_left_date + line_graph_date_denom/4 , line_graph_left_date + line_graph_date_denom/2, line_graph_left_date + 0.75 * line_graph_date_denom, line_graph_right_date].uniq,
-        [],
-        [min,0,max],
-        benchmarks_with_goal.collect{|b| "#{b.benchmark}-  #{b.grade_level}"},
-      ]
-
-
-    end
-
-    def line_graph_data(probes_for_this_graph)
-      a=[]
-      lg_dates=scaled_dates(probes_for_this_graph)
-      a << lg_dates
-      a << probes_for_this_graph.collect(&:score)
-      if @goal
-        a << [lg_dates.first, (@end_date - line_graph_left_date).to_i]
-        a << [probes_for_this_graph.first.score, @goal]
-      end
-      a
-    end
-
-    #TODO
-    def line_graph_left_date
-      (probes_for_graph.collect(&:administered_at) |[@first_date]).min
-    end
-
-    def line_graph_right_date
-      (probes_for_graph.collect(&:administered_at) |[@end_date]).max
-    end
-
-    def line_graph_date_denom
-      scale_denom=line_graph_right_date - line_graph_left_date
-      scale_denom = 0.0001 if scale_denom.zero?
-      scale_denom
-    end
-    def scaled_dates pp
-      pp.collect{|e| (e.administered_at - line_graph_left_date).to_i}
-    end
+  def line_graph_date_denom
+    scale_denom=line_graph_right_date - line_graph_left_date
+    scale_denom = 0.0001 if scale_denom.zero?
+    scale_denom
+  end
+  def scaled_dates pp
+    pp.collect{|e| (e.administered_at - line_graph_left_date).to_i}
   end
 end
