d064856f2813b94f4795f876e7c0fe8b659c70b6
Shawn Balestracci <shawn@vegantech.com>
2012-01-08 23:12:54 +0000

    no limit on number of selected students anymore

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -30,12 +30,7 @@ class StudentsController < ApplicationController
       flash[:notice] = 'No students selected'
     # elsif authorized_student_ids.to_set.subset?(params[:id].to_set)
     elsif params[:id].to_set.subset?(authorized_student_ids.to_set)
-      max_students = 200
       params[:id].uniq!
-      if params[:id].length > max_students
-        flash[:notice] ="Selection limited to #{max_students} students"
-        params[:id] = params[:id][0...max_students]
-      end
       self.selected_student_ids = params[:id]
       session[:selected_student] = selected_student_ids.first
       redirect_to student_url(session[:selected_student]) and return
