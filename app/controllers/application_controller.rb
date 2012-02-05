3076a88704efe2046f96c8e56c15c3b8f414e169
Shawn Balestracci <shawn@vegantech.com>
2012-02-05 13:47:21 -0800

    upgrading to rails3, in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,9 @@ class ApplicationController < ActionController::Base
   filter_parameter_logging :password
 
   before_filter :fixie6iframe,:options_for_microsoft_office_protocol_discovery,:authenticate, :authorize#, :current_district
+  def to_s
+    "wtf"
+  end
 
   SUBDOMAIN_MATCH=/(^sims$)|(^sims-open$)/
   private
@@ -229,5 +232,9 @@ def check_student
   def  handle_unverified_request
     raise ActionController::InvalidAuthenticityToken #for now
     super
+  end
+
+  def current_subdomain
+     request.subdomain
   end
 end
