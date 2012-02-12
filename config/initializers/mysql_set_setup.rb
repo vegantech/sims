a6a630e0a1211564784c52b206731d4d2db6c52f
Shawn Balestracci <shawn@vegantech.com>
2012-02-12 08:47:30 -0800

    added mysql sets as lib

---
+++ config/initializers/mysql_set_setup.rb
@@ -0,0 +1,10 @@
+#require 'mysql_set'
+ActiveRecord::ConnectionAdapters::TableDefinition.send :include, MysqlSet::TableDefinition
+ActiveRecord::ConnectionAdapters::Mysql2Column.send :include, MysqlSet::MysqlColumn
+ActiveRecord::ConnectionAdapters::Mysql2Adapter.send :include, MysqlSet::Mysql2Adapter
+
+silence_warnings do
+    c=ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES.merge(:set=>{:name=>'set'})
+      ActiveRecord::ConnectionAdapters::Mysql2Adapter.const_set("NATIVE_DATABASE_TYPES",c)
+end
+
