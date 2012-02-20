2948f8e73b6013ff33c92aa3f02d945e3691eebb
Shawn Balestracci <shawn@vegantech.com>
2012-02-20 10:01:45 -0800

    update branchs for staging and production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@ server "sims.widpi.managedmachine.com", :app, :web, :db, :primary => true
 
 set :domain, 'simspilot.org'
 set :default_url, 'https://simspilot.org'
+set :branch, "rails-2.3-pre-upgrade"
 
 set :login_note, '<p>Use the username and password assigned by your district admin.  It may be the same as your SIS.  Be sure to pick your district if you see the district dropdown.  If you\'re looking for the demo, it\'s at <%=link_to "http://sims-open.vegantech.com", "http://sims-open.vegantech.com" %> </p>'
 
