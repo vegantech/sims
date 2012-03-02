f1a8381ee86c99536e677220d13feefe5b55690b
Shawn Balestracci <shawn@vegantech.com>
2012-03-02 11:30:56 -0800

    fix some html escaping, update rails version, fix spec

--- spec/lib/daily_jobs_spec.rb
+++ spec/lib/daily_jobs_spec.rb
@@ -1,22 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe CreateInterventionPdfs do
+describe DailyJobs do
   describe 'self.run' do
-    it 'should call other methods' do
-      DailyJobs.should_receive(:regenerate_intervention_reports)
+    it 'should have specs' do
       DailyJobs.run
-    end
-
-  end
-
-  describe 'self.regenerate_intervention_reports' do
-    it 'should call CreateInterventionPdfs.generate for each district' do
-      District.should_receive(:all).and_return([1])
-      CreateInterventionPdfs.should_receive(:generate).with(1)
-      DailyJobs.regenerate_intervention_reports
+      pending
     end
   end
 
-
+  describe 'self.run_weekly' do
+    it 'should have spec' do
+      DailyJobs.run_weekly
+      pending
+    end
+  end
 end
 
