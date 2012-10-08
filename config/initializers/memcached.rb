6799929b26a1f468b58528e9be6d2134feffa614
Shawn Balestracci <shawn@vegantech.com>
2012-10-08 14:41:44 -0700

    another memcached try

--- config/initializers/memcached.rb
+++ config/initializers/memcached.rb
@@ -1,6 +1,6 @@
 begin
   require 'memcache'
-  MEMCACHE = MemCache.new('localhost:11211', :timeout => nil)
+  MEMCACHE = MemCache.new('localhost:11211', :timeout => 1)
   unless MEMCACHE.servers.first.alive?
     puts 'MEMCACHE SERVER DEAD' + MEMCACHE.servers.inspect
     MEMCACHE = nil
