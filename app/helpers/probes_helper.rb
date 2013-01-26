877fca091dfcf2ef111b1b2db1fedc32b33d9dba
Shawn Balestracci <shawn@vegantech.com>
2013-01-25 22:14:58 -0800

    Remove the ProbeQuestion Assessment Form which has not been used since MMSD SIMS < 2009

--- app/helpers/probes_helper.rb
+++ app/helpers/probes_helper.rb
@@ -20,18 +20,6 @@ module ProbesHelper
       graph.graph).html_safe
   end
 
-
-  def display_assessment_links(probe_assignment)
-    s=''
-    if !probe_assignment.probe_definition.probe_questions.blank?
-      s= link_to("Administer Assessment", new_assessment_probes_path(@intervention,probe_assignment)) + '<br />' 
-      if !probe_assignment.probes.blank? and !probe_assignment.probes.last.probe_questions.blank?
-        s+=link_to( "Update Assessment", update_assessment_probes_path(@intervention,probe_assignment)) + '<br />'
-      end
-    end
-    s
-  end
-
   def preview_graph_link(graph_type, intervention, probe_assignment, probe_assignment_counter)
     link_to "Preview #{graph_type.humanize} Graph", preview_graph_url(:intervention_id => intervention.id,
                                                                       :id => probe_assignment.id,
