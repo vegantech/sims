fc330965a0e2053a06d522ceb8266a4713747461
Shawn Balestracci <shawn@vegantech.com>
2013-03-10 11:03:24 -0700

    fix cookie signing key, I did not realize this changed in rails 3 and was using the wrong one this whole time! [ci skip]

--- config/initializers/host_info.rb
+++ config/initializers/host_info.rb
@@ -1,33 +1,9 @@
-
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Sims::Application.config.session_store :active_record_store
-
-
 #SIMS_DOMAIN = #sims-open.vegantech.com
 SIMS_PROTO="http"  #change to https when we're using that.
 #DEFAULT_URL = #'http://www.simspilot2.org:3000'
 
-
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-#
-#
-secret_file = Rails.root.join("config","secret")
-if File.exist?(secret_file)
-  secret=File.read(secret_file)
-else
-  secret = '9df9dcab8db4a1ccd72c79b014440d19d31042f8f6bcbec04c6c940c034d1cb210073b5613d0873544133aef0e5c21f197018d9a7ed23afbef31bc52b38c68ad'
-end
 sessionhash= {
     :key         => '_sims-open4_session',
-    :secret      => secret,
     :secure => (Rails.env.production? || Rails.env.staging? )
     }
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sims::Application.config.secret_token = '14df16f9d3940c20553652fe8fd4a3690f7eb0206316de64ee304e9f73fe721c27463444bc4be96f7e93194891868ea9c85d605a5daed7da9651ad3f35086d96'
+
+
+
+secret_file = Rails.root.join("config","secret")
+if File.exist?(secret_file)
+  secret=File.read(secret_file)
+else
+  secret = '63d92d8011a5bdfe67367f34f2b588f4642f8961a0bc8fb07268234535540748b27a60fe4dfd38f1ff6c7fed71de9e6ffbb4c926d745de31fef048ba42b23587'
+end
+Sims::Application.config.secret_token =  secret
+
