49cdf2dac75009f0973656d3db97eae45ac88963
Shawn Balestracci <shawn@vegantech.com>
2013-03-28 15:08:41 -0700

    switch to errbit from airbrake [ci skip]

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,10 @@
 Airbrake.configure do |config|
-  config.api_key = '7fed52e49c9cf366087d9071db425f1c'
+  config.api_key = '7a09837280d1249c9b7c944051697717'
+  config.host    = 'vegantech-errbit.herokuapp.com'
+  config.port    = 443
+  config.secure  = config.port == 443
+
+  #config.api_key = '7fed52e49c9cf366087d9071db425f1c'
   config.ignore_only  = []
   config.ignore_user_agent << 'Microsoft Office Protocol Discovery'
   config.development_environments << 'development_with_cache'
