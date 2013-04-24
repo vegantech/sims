c00896a0de00325564ac80b100d50a2dca1de01b
Shawn Balestracci <shawn@vegantech.com>
2013-04-24 14:25:54 -0700

    extracted checklist scorer class from checklist method

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -112,43 +112,15 @@ class Checklist < ActiveRecord::Base
   end
 
   def score_checklist
-    @score_results=Hash.new{|h,k| h[k]={}}
     #for demo purpose, answering the second question will pass the checklist
     if student and student.last_name=="Flag" and student.first_name="Every" and answers.find_by_answer_definition_id(6) then
       self.promoted=true
       return true
     end
 
-    inapplicable_questions=[]
-    # all 1-8 must be answered, all 9 must have some content common to all tiers
-      checklist_definition_cache.element_definitions.each do |element|
-          if element.kind=="scale" and !element_definitions_for_answers.include?(element) then
-            @score_results[element.question_definition][element] = "A value must be picked"
-          elsif element.kind=="sa" and !element_definitions_for_answers.include?(element) then
-            @score_results[element.question_definition][element]= "Text must be entered"
-          elsif element.kind == "applicable"
-            ad=element.answer_definitions.find_by_value(0).id
-            @score_results[element.question_definition][element] = "A value must be picked" if !element_definitions_for_answers.include?(element)
-            inapplicable_questions << element.question_definition  if answers.collect(&:answer_definition_id).include?(ad)
-          end #if
-        end # do
-
-    case from_tier
-    when 0,1
-      nil
-    when 2,3
-      #All question 9 completed (done above),  Questions 1-8 needs an answer of [2,3] or better
-      answers.each do |answer|
-        @score_results[answer.answer_definition.element_definition.question_definition][answer.answer_definition.element_definition] =
-          "Need to score #{from_tier} or better." if answer.answer_definition.value <"#{from_tier}" and answer.answer_definition.element_definition.kind=="scale"
-      end #do
-    end # case
-
-    inapplicable_questions.each do |iq|
-      @score_results.delete(iq)
-    end
+    @score_results = ChecklistScorer.new(self).score
     self.promoted = @score_results.blank?
-  end #method
+  end
 
   def element_definitions_for_answers
     @edfa||= answers.collect(&:answer_definition).collect(&:element_definition)
