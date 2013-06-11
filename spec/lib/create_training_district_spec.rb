4f1722a8a6c8e669b2228e0e9a12e307ec3c0a5b
Shawn Balestracci <shawn@vegantech.com>
2013-06-11 16:31:30 -0700

    add spec for create training district

--- spec/lib/create_training_district_spec.rb
+++ spec/lib/create_training_district_spec.rb
@@ -1,6 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe CreateTrainingDistrict do
+  describe 'generate' do
+    it 'should generate 20 districts plus the named ones' do
+      CreateTrainingDistrict.should_receive(:generate_one).exactly(20).times
+      CreateTrainingDistrict.should_receive(:generate_named_districts)
+      CreateTrainingDistrict.generate
+    end
+  end
   describe 'generate_one class method' do
     before :all do
       GoalDefinition.delete_all
