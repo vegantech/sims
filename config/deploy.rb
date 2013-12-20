b7506ce9be458a6ebb6aee877646beed789aaa4f
Shawn Balestracci <shawn@vegantech.com>
2013-12-19 21:45:10 -0800

    adjust gc settings for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -141,7 +141,7 @@ end
 require 'airbrake/capistrano'
 
 set :default_environment, self[:default_environment].merge(
-'RUBY_GC_MALLOC_LIMIT'=>100000000,
+'RUBY_GC_MALLOC_LIMIT'=>10000000,
 'RUBY_FREE_MIN'=>500000,
 'RUBY_HEAP_MIN_SLOTS'=>800000,
 'RUBY_HEAP_SLOTS_INCREMENT'=>300000,
