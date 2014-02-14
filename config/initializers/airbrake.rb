f589bb0e08c6a0927c8f198da73651d6e61bc001
Shawn Balestracci <shawn@vegantech.com>
2014-02-14 10:54:49 -0800

    [#785 state:resolved] fix for ssl problem needs to be handled on server side as well

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -3,6 +3,7 @@ Airbrake.configure do |config|
   config.host    = 'vegantech-errbit.herokuapp.com'
   config.port    = 443
   config.secure  = config.port == 443
+  config.use_system_ssl_cert_chain = true
 
   #config.api_key = '7fed52e49c9cf366087d9071db425f1c'
   config.ignore_only  = []
