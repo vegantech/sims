021fe0c35047dba0dbdb112fce919485c4e993dd
Shawn Balestracci <shawn@vegantech.com>
2014-03-06 22:30:34 -0800

    move score comment to its own file

--- app/models/grouped_progress_entry.rb
+++ app/models/grouped_progress_entry.rb
@@ -54,12 +54,6 @@ class GroupedProgressEntry
     raise param.inspect
   end
 
-  def end_interventions!(end_ids, reason, fidelity)
-    student_interventions.each do |i|
-      i.end(@user, reason, fidelity) if Array(end_ids).include?(i.id.to_s)
-    end
-  end
-
   def update_attributes(param)
     participants = param.delete("participant_user_ids") || []
     param.each do |int_id, int_attr|
@@ -169,63 +163,42 @@ class GroupedProgressEntry
 
 
 
-  class ScoreComment
-    attr_accessor :date,:score,:comment,:intervention,:id,:numerator,:denominator
-    delegate :end, :to => :intervention
-    def initialize(intervention,user)
-      @intervention = intervention
-      @id = intervention.id
-      @user = user
-      @comment = ''
-      @errors =''
-      @probe = nil
-      @score = nil
-    end
-
-    def errors
-      @errors.to_s
-    end
-
-    def update_attributes(params)
-      @comment = params['comment']
-      @intervention.comment_author=@user.id
-      @intervention.comments_attributes = {"0" => {:comment => @comment}}
-      begin
-        @date = Date.civil(params["date(1i)"].to_i,params["date(2i)"].to_i,params["date(3i)"].to_i)
-      rescue ArgumentError
-        @errors +='Invalid Date'
-      end
-      @score = params[:score]
 
 
-      @probe=@intervention.intervention_probe_assignment.probes.build(:score => @score, :administered_at => @date) unless @score.blank?
-    end
 
-    def valid?
-      if @intervention.valid? && (!@probe || @probe.valid?)  && @errors.blank?
-        true
-      else
-        @errors += @intervention.errors.full_messages.join(", ") +' ' + @probe.errors.full_messages.join(", ")
-        false
-      end
-    end
 
-    def save
-      @intervention.save!
-      @probe.save! unless @probe.blank?
-    end
 
-    def student
-      @intervention.student
-    end
-    def to_param
-      @id
-    end
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   def persisted?
     true
   end
+
+  def end_interventions!(end_ids, reason, fidelity)
+    student_interventions.each do |i|
+      i.end(@user, reason, fidelity) if Array(end_ids).include?(i.id.to_s)
+    end
+  end
+
+
 
 private
   def self.interventions(id)
