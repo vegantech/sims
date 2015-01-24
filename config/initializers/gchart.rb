4fd28b73978d1a04a4230848d5cce5bfe8a11621
Shawn Balestracci <shawn@vegantech.com>
2015-01-23 19:23:05 -0800

    reduce duplication in gchart bugfix initializer

--- config/initializers/gchart.rb
+++ config/initializers/gchart.rb
@@ -1,17 +1,22 @@
 class Gchart
   attr_accessor :chds
   def self.url
-   '/chart?'
+    '/chart?'
   end
 
 
   def text_encoding
-     chds = @chds || dataset.map{|ds| "#{ds[:min_value]},#{ds[:max_value]}" }.join(",")
-     if custom and custom.include?("&chds")
-       "t" + number_visible + ":" + datasets.map{ |ds| ds.map{|e|e||'_'}.join(',') }.join('|') #+ "&chds=" + chds
-     else
-       "t" + number_visible + ":" + datasets.map{ |ds| ds.map{|e|e||'_'}.join(',') }.join('|') + "&chds=" + chds
-     end
+    chds = @chds || dataset.map{|ds| "#{ds[:min_value]},#{ds[:max_value]}" }.join(",")
+    if custom and custom.include?("&chds")
+      text_encoding_without_chds
+    else
+      text_encoding_without_chds + "&chds=" + chds
+    end
+  end
+
+  private
+  def text_encoding_without_chds
+    "t" + number_visible + ":" + datasets.map{ |ds| ds.map{|e|e||'_'}.join(',') }.join('|')
   end
 
 end
