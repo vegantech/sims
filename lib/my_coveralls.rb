8e267b18f99dcacf9342ded23f6e7ca04ad5aeac
Shawn Balestracci <shawn@vegantech.com>
2015-01-25 21:14:02 -0800

    move my coveralls plugin (which requires everything) to lib

--- lib/my_coveralls.rb
+++ lib/my_coveralls.rb
@@ -8,15 +8,10 @@ class MyCoveralls
         obj = ruby_file.split("#{target_dir}/",2).last.split(".rb").first
         obj = obj.split("/",2).last if target_dir == "/app"
         begin
-          # trigger the normal Rails mechanism to require files
-          if obj == "application" && target_dir.include?("controller")
-            ApplicationController
-          else
-            obj.classify.constantize
-          end
+          obj.classify.constantize
         rescue NameError, LoadError
           #app/manifests are used with moonshine, but not needed during project execution
-           require ruby_file unless ruby_file.include?('app/manifests')
+           require ruby_file
         end
       end
     end
