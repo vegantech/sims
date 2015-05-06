75b12801bffde8102520fd845f7ae4d08edb2dfd
Shawn Balestracci <shawn@vegantech.com>
2015-05-05 17:43:28 -0700

    backport for protected attributes

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Sims
      )
      config.active_record.observers = :principal_override_observer , :recommendation_observer
 
+     config.active_record.whitelist_attributes = false
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
